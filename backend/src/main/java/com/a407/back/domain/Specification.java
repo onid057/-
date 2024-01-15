5b6dcdb47b9787ce21aee81c2f83c6bd1a10dfd5
seri <yerioo@naver.com>
2024-01-15 17:22:37 +0900

    add: domainSpecification

---
+++ backend/src/main/java/com/a407/back/domain/Specification.java
@@ -0,0 +1,37 @@
+package com.a407.back.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Specification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Builder
+    public Specification(Long id, String password, String email) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+    }
+}
+
