8f8b092dc9c8d58800382cc0d5a66e9c982ee004
장수민 <jsm0726@naver.com>
2024-01-17 12:35:25 +0900

    feat: 여러 화면에서 공통으로 사용하는 Notice 컴포넌트 생성

---
+++ frontend/src/components/common/Notice.jsx
@@ -0,0 +1,43 @@
+// 공통 알림 컴포넌트
+
+import { styled } from 'styled-components';
+
+const NoticeWrapper = styled.div`
+	width: ${props => props.$width};
+	margin: ${props => (props.$margin ? props.$margin : 0)};
+	padding: ${props => props.$padding};
+	background-color: yellow;
+	border-radius: 25px;
+`;
+
+// 추후에 width(너비) 수정 필요
+const FlexWrapper = styled.div`
+	width: 250px;
+	margin: 0 auto;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const TextWrapper = styled.div`
+	width: 250px;
+	margin: 0 auto;
+	text-align: left;
+`;
+
+// upper에 컴포넌트 목록을 배열로 전달
+// lower에 텍스트 전달
+export function Notice({ width, padding, margin, upper, lower }) {
+	return (
+		<NoticeWrapper $width={width} $margin={margin} $padding={padding}>
+			{upper && (
+				<FlexWrapper>
+					{upper.map(component => (
+						<div>{component}</div>
+					))}
+				</FlexWrapper>
+			)}
+			{lower && <TextWrapper>{lower}</TextWrapper>}
+		</NoticeWrapper>
+	);
+}
