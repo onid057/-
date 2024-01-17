606b4f9e57b665274aeb5684ea0a685fb818a9fa
waterlee6 <waterleee6@gmail.com>
2024-01-17 17:56:03 +0900

    test: counter 함수 테스트 작성

--- frontend/public/practice.jsx
+++ frontend/public/practice.jsx
@@ -7,3 +7,11 @@ console.log('hihi');
 function name(params) {
 	console.log('dd');
 }
+
+// 20240117 count 함수 만들어보기
+import { useState } from 'react';
+
+function counter() {
+	const [cnt, setCnt] = useState(0);
+	return <button onClick={() => setCnt(cnt + 1)}>하나더하기!</button>;
+}
