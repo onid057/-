0dc48d8d7662b51548a27ac65aab6dc7e1631446
황준식 <wnstlr0908@naver.com>
2024-01-17 17:47:42 +0900

    test: 카운터 예제 js 파일 작성

--- frontend/public/practice.js
+++ frontend/public/practice.js
@@ -1,7 +1,32 @@
 // console.log('first-practice')
 
-console.log('single-quote-test');
-console.log('semi-colon-test');
-function test() {
-	console.log('function-test');
+// console.log('single-quote-test');
+// console.log('semi-colon-test');
+// function test() {
+// 	console.log('function-test');
+// }
+
+import React, { useState } from 'react';
+
+function practice() {
+	const [count, setCount] = useState(0);
+
+	const onIncrease = () => {
+		setCount(count => count + 1);
+	};
+
+	const onDecrease = () => {
+		setCount(count => count - 1);
+	};
+
+	return (
+		<>
+			<h1>practice</h1>
+			<h2>{count}</h2>
+			<button onClick={onIncrease}>+1</button>
+			<button onClick={onDecrease}>-1</button>
+		</>
+	);
 }
+
+export default practice;
