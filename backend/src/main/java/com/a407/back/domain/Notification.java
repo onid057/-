107ccd2a62a2f9ea602344f54bf931ec9913a879
강태연 <kangty1028@naver.com>
2024-01-18 16:35:35 +0900

    add: domainNotification

---
+++ backend/src/main/java/com/a407/back/domain/Notification.java
@@ -0,0 +1,54 @@
+package com.a407.back.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Table(name = "NOTIFICATION")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id", updatable = false)
+    private Long notificationId;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room roomId;
+
+    @Column(name = "content", nullable = false, length = 200)
+    private String content;
+
+    @ColumnDefault("false")
+    @Column(name = "is_read", nullable = false)
+    private boolean isRead;
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private Timestamp createdAt;
+
+    @Builder
+    public Notification(Long notificationId, Room roomId, String content, boolean isRead,
+        Timestamp createdAt) {
+        this.notificationId = notificationId;
+        this.roomId = roomId;
+        this.content = content;
+        this.isRead = isRead;
+        this.createdAt = createdAt;
+    }
+}
