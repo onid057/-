61372dc768ecf12ab7ec72e8aa2b3e63c2c4d949
김세리 <yerioo@naver.com>
2024-01-18 16:23:56 +0900

    add: domainComplain

---
+++ backend/src/main/java/com/a407/back/domain/Complain.java
@@ -0,0 +1,46 @@
+package com.a407.back.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Table(name = "COMPLAIN")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Complain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "complain_id", updatable = false)
+    private long complainId;
+
+    @OneToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room roomId;
+
+    @Column(name = "content", nullable = false, length = 50)
+    private String content;
+
+    @ColumnDefault("false")
+    @Column(name = "is_processed", nullable = false)
+    private boolean isProcessed;
+
+    @Builder
+    public Complain(long complainId, Room roomId, String content, boolean isProcessed) {
+        this.complainId = complainId;
+        this.roomId = roomId;
+        this.content = content;
+        this.isProcessed = isProcessed;
+    }
+}
