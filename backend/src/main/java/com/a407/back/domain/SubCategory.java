48e79edeff31c0492a01a8c88fcbe3b9c38b6621
강태연 <kangty1028@naver.com>
2024-01-18 16:34:53 +0900

    add: domainSubCategory

---
+++ backend/src/main/java/com/a407/back/domain/SubCategory.java
@@ -0,0 +1,40 @@
+package com.a407.back.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "SUB_CATEGORY")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class SubCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sub_category_id", updatable = false)
+    private Long subCategoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "major_category_id", nullable = false)
+    private MajorCategory majorCategoryId;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Builder
+    public SubCategory(Long subCategoryId, MajorCategory majorCategoryId, String name) {
+        this.subCategoryId = subCategoryId;
+        this.majorCategoryId = majorCategoryId;
+        this.name = name;
+    }
+}
