7d77df825400faf9b918eda9063f81cbcb449f89
김세리 <yerioo@naver.com>
2024-01-18 16:59:14 +0900

    feat: email로 사용자 찾기

--- backend/src/main/java/com/a407/back/model/repo/basicRepositoryImpl.java
+++ backend/src/main/java/com/a407/back/model/repo/basicRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.a407.back.model.repo;
 
+import com.a407.back.domain.QUser;
 import com.a407.back.domain.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -8,10 +9,20 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class basicRepositoryImpl implements basicRepository{
+public class basicRepositoryImpl implements basicRepository {
+
     private final JPAQueryFactory query;
 
     private final EntityManager em;
+
+    @Override
+    public User findByUserEmail(String email) {
+        //하나만 반환->fetchOne
+        //Entity Manager는 다른걸로 find해야함.
+        QUser qUser = QUser.user;
+        return query.select(qUser).from(qUser).where(qUser.email.eq(email)).fetchOne();
+    }
+
     @Override
     public User save(User user) {
         em.persist(user);
--- backend/src/main/java/com/a407/back/model/service/basicService.java
+++ backend/src/main/java/com/a407/back/model/service/basicService.java
@@ -1,19 +1,23 @@
 package com.a407.back.model.service;
 
 import com.a407.back.domain.User;
-import com.a407.back.model.repo.basicRepository;
+import com.a407.back.exception.CustomException;
+import com.a407.back.config.ErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import com.a407.back.model.repo.basicRepository;
 
 @Service
 @RequiredArgsConstructor
 public class basicService {
+
     private final basicRepository basicRepository;
+
     @Transactional
-    public int save(User user) {
-        return basicRepository.save(user).getId();
+    public Long save(User user) {
+        // 에러 처리
+        if(basicRepository.findByUserEmail(user.getEmail()) != null) throw new CustomException(ErrorCode.INVALID_PARAMETER);
+        return basicRepository.save(user).getUserId();
     }
 }
