8112c5abd15c12e8a91e3f625c51a3461a367f4f
김세리 <yerioo@naver.com>
2024-01-18 16:28:54 +0900

    fix: dtoUserRequest

--- backend/src/main/java/com/a407/back/dto/UserRequest.java
+++ backend/src/main/java/com/a407/back/dto/UserRequest.java
@@ -1,24 +1,54 @@
 package com.a407.back.dto;
 
+import com.a407.back.domain.Association;
 import com.a407.back.domain.User;
+import com.a407.back.domain.User.Gender;
+import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class UserRequest {
-    private long id;
-    private String password;
+    private long userId;
+    private Association associationId;
     private String email;
+    private String password;
+    private String name;
+    private Timestamp birth;
+    private Gender gender;
+    private String address;
+    private byte[] profileImage;
+    private boolean isCertificated;
+    private double latitude;
+    private double longitude;
+    private String account;
+    private boolean isBlocked;
+    private boolean isAdmin;
+    private boolean isAffiliated;
+    private int serviceCount;
 
     public User toEntity() {
         return User.builder()
-            .id(id)
-            .password(password)
+            .userId(userId)
+            .associationId(associationId)
             .email(email)
+            .password(password)
+            .name(name)
+            .birth(birth)
+            .gender(gender)
+            .address(address)
+            .profileImage(profileImage)
+            .isCertificated(isCertificated)
+            .latitude(latitude)
+            .longitude(longitude)
+            .account(account)
+            .isBlocked(isBlocked)
+            .isAdmin(isAdmin)
+            .isAffiliated(isAffiliated)
+            .serviceCount(serviceCount)
             .build();
     }
 }
