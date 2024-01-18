e262a3050bd463d29a7e78f73af811658c242636
김세리 <yerioo@naver.com>
2024-01-18 16:22:28 +0900

    add: domainAssociation

---
+++ backend/src/main/java/com/a407/back/domain/Association.java
@@ -0,0 +1,41 @@
+package com.a407.back.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Table(name = "ASSOCIATION")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Association {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "association_id", updatable = false)
+    private Long associationId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private Timestamp createdAt;
+
+    @Builder
+    public Association(Long associationId, Long userId, Timestamp createdAt) {
+        this.associationId = associationId;
+        this.userId = userId;
+        this.createdAt = createdAt;
+    }
+}
