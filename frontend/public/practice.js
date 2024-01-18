ffc8cf0560f926ee3aefc5a6dd0f05b1fb641a67
장수민 <jsm0726@naver.com>
2024-01-18 10:32:04 +0900

    style: prettier 설정 변경

--- frontend/public/practice.js
+++ frontend/public/practice.js
@@ -9,24 +9,24 @@
 import React, { useState } from 'react';
 
 function practice() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	const onIncrease = () => {
-		setCount(count => count + 1);
-	};
+  const onIncrease = () => {
+    setCount(count => count + 1);
+  };
 
-	const onDecrease = () => {
-		setCount(count => count - 1);
-	};
+  const onDecrease = () => {
+    setCount(count => count - 1);
+  };
 
-	return (
-		<>
-			<h1>practice</h1>
-			<h2>{count}</h2>
-			<button onClick={onIncrease}>+1</button>
-			<button onClick={onDecrease}>-1</button>
-		</>
-	);
+  return (
+    <>
+      <h1>practice</h1>
+      <h2>{count}</h2>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </>
+  );
 }
 
 export default practice;
