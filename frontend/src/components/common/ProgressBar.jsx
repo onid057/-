bdb61748148e5c113297febe9b9d6691116af414
waterlee6 <waterleee6@gmail.com>
2024-01-19 16:52:45 +0900

    S10P12A407-78 fix: ProgressBar 상단 글자 제거

--- frontend/src/components/common/ProgressBar.jsx
+++ frontend/src/components/common/ProgressBar.jsx
@@ -9,44 +9,6 @@ const ProgressBarWrapper = styled.div`
   gap: 5px;
 `;
 
-const BarText = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  font-size: 13px;
-`;
-
-// 1번 필터에서는 LeftTextBox가 출력되지 않았으면 좋겠습니다.
-const LeftTextBox = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-// 6번 필터에서는 RightTextBox가 출력되지 않았으면 좋겠습니다.
-const RightTextBox = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const LeftArrow = styled.div`
-  width: 12px;
-  height: 8px;
-  background-image: url('/images/left_arrow_no_tail.svg');
-  background-repeat: no-repeat;
-  background-size: auto 100%;
-  background-position: center;
-`;
-
-const RightArrow = styled.div`
-  width: 12px;
-  height: 8px;
-  background-image: url('/images/right_arrow_no_tail.svg');
-  background-repeat: no-repeat;
-  background-size: auto 100%;
-  background-position: center;
-`;
-
 const BarGage = styled.progress`
   width: 100%;
   height: 16px;
@@ -66,17 +28,6 @@ const BarGage = styled.progress`
 function ProgressBar() {
   return (
     <ProgressBarWrapper>
-      <BarText>
-        <LeftTextBox>
-          <LeftArrow></LeftArrow>
-          <span>이전</span>
-        </LeftTextBox>
-        <RightTextBox>
-          <span>다음</span>
-          <RightArrow></RightArrow>
-        </RightTextBox>
-      </BarText>
-
       {/* 아래 value 값에 따라 진척률이 변경됩니다. */}
       <BarGage value={20} max={100}></BarGage>
     </ProgressBarWrapper>
