0c21e156bbe1d35004ee4fb4d804fc3b177c500b
장수민 <jsm0726@naver.com>
2024-01-19 16:40:57 +0900

    S10P12A407-95 fix: Image 컴포넌트에 width, height prop 전달

--- frontend/src/components/common/Image.jsx
+++ frontend/src/components/common/Image.jsx
@@ -1,16 +1,18 @@
 import { styled } from 'styled-components';
 
 const ImageWrapper = styled.div`
-  width: 100px;
-  height: 100px;
+  width: ${props => props.$width};
+  height: ${props => props.$height};
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
   background-image: url(${props => props.$src});
 `;
 
-function Image({ src }) {
-  return <ImageWrapper $src={src}></ImageWrapper>;
+function Image({ src, width, height }) {
+  return (
+    <ImageWrapper $src={src} $width={width} $height={height}></ImageWrapper>
+  );
 }
 
 export default Image;
