9419232f235ce2a5725829b83e9919f1bd3d600c
waterlee6 <waterleee6@gmail.com>
2024-01-19 16:55:49 +0900

    S10P12A407-78 style: prettier 설정 덮어쓰기

--- frontend/src/pages/TestLee.jsx
+++ frontend/src/pages/TestLee.jsx
@@ -2,12 +2,15 @@ import { styled } from 'styled-components';
 import { Input } from '../components/common/Input';
 import { Notice } from '../components/common/Notice';
 import ProgressBar from '../components/common/ProgressBar';
-import DateTimeInput from '../components/common/DateTimeInput';
+import DateTimeInputInput from '../components/common/DateTimeInputInput';
+import DateTimeInputSelect from '../components/common/DateTimeInputSelect';
+import AddressInput from '../components/common/AddressInput';
+import LongInputBox from '../components/common/LongInputBox';
 
 const TestLeeWrapper = styled.div`
   width: 320px;
-  height: 568px;
-  margin: 0 auto 20px;
+  height: auto;
+  margin: 0 auto;
   padding: 10% 0;
   display: flex;
   flex-direction: column;
@@ -16,26 +19,47 @@ const TestLeeWrapper = styled.div`
   line-height: 1.3;
 `;
 
+const H2title = styled.h2`
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  background-color: #7e7e7e;
+  font-size: small;
+  color: white;
+`;
+
 function TestLee() {
   return (
     <TestLeeWrapper>
       <h1 style={{ fontWeight: 'bold' }}>수민이(이)의 테스트 페이지</h1>
       <hr style={{ marginBottom: '30px' }}></hr>
 
-      <h2>수민이가 만든 Input 컴포넌트</h2>
-      <Input></Input>
+      <H2title>수민이가 만든 Input 컴포넌트</H2title>
+      <Input
+        width="90%"
+        labelText="ddd"
+        placeholder="장수민"
+        commentText={'dsfsf'}
+      ></Input>
       <hr style={{ marginBottom: '30px' }}></hr>
 
-      <h2>수민이가 만든 Notice 컴포넌트</h2>
+      <H2title>수민이가 만든 Notice 컴포넌트</H2title>
       <Notice></Notice>
       <hr style={{ marginBottom: '30px' }}></hr>
 
-      <h2>프로그래스 바 만들기</h2>
+      <H2title>프로그래스 바 만들기</H2title>
       <ProgressBar></ProgressBar>
       <hr style={{ marginBottom: '30px' }}></hr>
 
-      <h2>DateTimeInput 만들기</h2>
-      <DateTimeInput></DateTimeInput>
+      <H2title>DateTimeInput 만들기</H2title>
+      <DateTimeInputInput></DateTimeInputInput>
+      <DateTimeInputSelect></DateTimeInputSelect>
+
+      <H2title>AddressInput 만들기</H2title>
+      <AddressInput></AddressInput>
+
+      <H2title>LongInputBox 만들기</H2title>
+      <LongInputBox></LongInputBox>
     </TestLeeWrapper>
   );
 }
