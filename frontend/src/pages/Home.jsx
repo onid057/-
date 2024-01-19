10223ebf4162d637f14ac3bf6975ce49c42b0d61
장수민 <jsm0726@naver.com>
2024-01-19 16:40:11 +0900

    S10P12A407-95 style: prettier 설정 덮어쓰기

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { styled } from 'styled-components';
 
 const HomeWrapper = styled.div`
-	background-color: yellow;
+  background-color: yellow;
 `;
 
 export default function Home() {
-	return <HomeWrapper>HOME 페이지 입니다!</HomeWrapper>;
+  return <HomeWrapper>HOME 페이지 입니다!</HomeWrapper>;
 }
