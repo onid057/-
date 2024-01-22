8c08bc152f239e14bb16ccc4e836d081843261f2
waterlee6 <waterleee6@gmail.com>
2024-01-22 17:47:33 +0900

    S10P12A407-78 feat: 주소 입력 받는 AddressInput.jsx 컴포넌트

--- frontend/src/components/common/AddressInput.jsx
+++ frontend/src/components/common/AddressInput.jsx
@@ -1,10 +1,11 @@
 import { styled } from 'styled-components';
-import React, { useEffect, useRef } from 'react';
-import input from './Input';
+import React, { useEffect, useRef, useState } from 'react';
+import Input from './Input';
+import Button from './Button';
 
 const AddressInputWrapper = styled.div`
   box-sizing: border-box;
-  width: 90%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -29,11 +30,12 @@ const TitleBox = styled.div`
   font-weight: light;
 `;
 
-const NoContentBox = styled.div`
+const InputBox = styled.div`
   box-sizing: border-box;
   width: 100%;
   height: 180px;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
   gap: 18px;
@@ -41,74 +43,63 @@ const NoContentBox = styled.div`
   font-size: 20px;
 `;
 
-const ContentBox = styled.div`
-  box-sizing: border-box;
+const InputBoxFirst = styled.div`
   width: 100%;
-  height: 60px;
-  padding: 0px 15px;
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  gap: 12px;
-  font-weight: light;
-  font-size: 16px;
-  color: gray;
-  background-color: #ffffff;
-  border-radius: 25px;
+  align-items: flex-end;
 `;
 
-const ExampleText = styled.div`
-  font-size: 18px;
-  font-weight: light;
-  opacity: 30%;
-`;
-
-function AddressInput() {
-  const postcodeRef = useRef();
-
+const AddressInput = () => {
   useEffect(() => {
+    // useEffect 내에서 다음 지도 API 스크립트를 동적으로 로드
     const script = document.createElement('script');
     script.src =
       '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
     script.async = true;
     document.body.appendChild(script);
 
-    script.onload = () => {
-      // 스크립트 로드 후에 실행할 코드
-      // 여기서는 우편번호 서비스를 초기화하고 팝업을 열도록 설정
-      postcodeRef.current = new window.daum.Postcode({
-        oncomplete: function (data) {
-          // 팝업에서 검색결과 항목을 클릭했을 때 실행할 코드
-          console.log('우편번호 정보:', data);
-        },
-      });
-      postcodeRef.current.open();
-    };
-
+    // 컴포넌트가 언마운트될 때 스크립트 제거
     return () => {
-      // 컴포넌트가 언마운트되면 스크립트를 제거
       document.body.removeChild(script);
     };
   }, []);
+
+  const handleOpenPopup = () => {
+    // 다음 지도 API 코드 호출
+    new window.daum.Postcode({
+      oncomplete: function (data) {
+        // 우편번호와 주소 데이터 출력
+        const postCode = data.zonecode;
+        const address = data.address + ' (' + data.buildingName + ')';
+        // console.log('검색 결과:', data);
+        console.log('우편번호:', postCode);
+        console.log('주소:', address);
+      },
+    }).open();
+  };
 
   return (
     <AddressInputWrapper>
       <InsideWrapper>
         <TitleBox>장소</TitleBox>
-        <NoContentBox>
-          {/* 우편번호 서비스를 트리거할 버튼 또는 다른 UI 요소를 추가 */}
-          <button onClick={() => postcodeRef.current.open()}>
-            우편번호 찾기
-          </button>
-        </NoContentBox>
-      </InsideWrapper>
-
-      <InsideWrapper>
-        <TitleBox>세부 장소와 방법</TitleBox>
-        <ContentBox></ContentBox>
+        <InputBox>
+          <InputBoxFirst>
+            <Input width={'128px'} placeholder="우편번호"></Input>
+            {/* 우편번호 서비스를 트리거할 버튼 또는 다른 UI 요소를 추가 */}
+            <Button
+              onClick={handleOpenPopup}
+              mode={'SMALL_WHITE'}
+              msg={'우편번호 찾기'}
+            ></Button>
+          </InputBoxFirst>
+          <Input width={'100%'} placeholder="주소"></Input>
+          {/* 상세 주소는 input에서 받음 */}
+          <Input width={'100%'} placeholder="상세 주소"></Input>
+        </InputBox>
       </InsideWrapper>
     </AddressInputWrapper>
   );
-}
+};
 
 export default AddressInput;
