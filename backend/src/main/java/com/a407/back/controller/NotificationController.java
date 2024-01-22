bb38819a39e8620be93c5ce0ce08f68072c24e0e
곽희웅 <gmldnd567@naver.com>
2024-01-22 17:08:05 +0900

    S10P12A407-95 feat: 제안 거절 및 방 해체

--- backend/src/main/java/com/a407/back/controller/NotificationController.java
+++ backend/src/main/java/com/a407/back/controller/NotificationController.java
@@ -4,6 +4,7 @@ import com.a407.back.domain.Notification;
 import com.a407.back.dto.UserNotificationResponse;
 import com.a407.back.dto.ZipsaNotificationResponse;
 import com.a407.back.model.service.NotificationService;
+import com.a407.back.model.service.RoomService;
 import com.a407.back.model.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,12 +21,13 @@ public class NotificationController {
 
     private final NotificationService notificationService;
     private final UserService userService;
+    private final RoomService roomService;
 
     @GetMapping("/{notificationId}")
-    public ResponseEntity<?> getNotificationDetail(@PathVariable("notificationId") Long notificationId) {
+    public ResponseEntity<?> getNotificationDetail(
+        @PathVariable("notificationId") Long notificationId) {
         Notification notification = notificationService.findByNotificationId(notificationId);
         boolean isZipsa = userService.isWorkedDistinction(notification.getReceiveId());
-        System.out.println(isZipsa);
         if (isZipsa) {
             ZipsaNotificationResponse zipsaNotificationResponse = notificationService.getZipsaNotification(
                 notification);
@@ -34,5 +36,22 @@ public class NotificationController {
         UserNotificationResponse userNotificationResponse = notificationService.getUserNotification(
             notification);
         return ResponseEntity.status(HttpStatus.OK).body(userNotificationResponse);
+    }
+
+    @GetMapping("/{notificationId}/rejection")
+    public ResponseEntity<Integer> rejectNotification(
+        @PathVariable("notificationId") Long notificationId) {
+        // 해당 알림을 reject
+        notificationService.changeNotificationStatusAcceptOrReject(notificationId, "reject");
+        // 방에서 알림 숫자를 줄임
+        Notification notification = notificationService.findByNotificationId(notificationId);
+        int newNotificationCount = roomService.reduceNotificationCount(
+            notification.getRoomId().getRoomId());
+        // 만약 알림 숫자를 줄였을 때 0이 된다면 방 broken으로 바꿈
+        if (newNotificationCount == 0) {
+            System.out.println(notification.getRoomId().getRoomId());
+            roomService.chageRoomStatus(notification.getRoomId().getRoomId(), "broken");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(newNotificationCount);
     }
 }
