25725d233bb7ea8a1f6528c643b3dca385c8f462
장수민 <jsm0726@naver.com>
2024-01-23 10:17:58 +0900

    S10P12A407-95 Feat: 회원가입 플로우에서 성별을 선택하는 페이지 생성

--- frontend/src/pages/registerFunnel/Gender.jsx
+++ frontend/src/pages/registerFunnel/Gender.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import { styled } from 'styled-components';
+
+import NavigationBar from '../../components/common/NavigationBar';
+import Image from '../../components/common/Image';
+import Paragraph from '../../components/common/Paragraph';
+import BoldText from '../../components/common/BoldText';
+import Button from '../../components/common/Button';
 
 const Wrapper = styled.div`
   width: 320px;
   min-height: 568px;
   margin: 0 auto;
-  padding: 30px 16px;
+  padding: 0 16px;
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -14,11 +21,55 @@ const Wrapper = styled.div`
   white-space: pre-wrap;
 `;
 
-function Gender({ onNext }) {
+const ButtonWrapper = styled.div`
+  margin: 60px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+function Gender({ onPrevious, onNext }) {
+  const [gender, setGender] = useState('');
+
   return (
     <Wrapper>
-      성별
-      <button onClick={onNext}>이름 입력으로 이동</button>
+      <NavigationBar
+        leftContent={
+          <Image
+            width="40px"
+            height="40px"
+            margin="0 0 0 -12px"
+            src={process.env.PUBLIC_URL + '/images/left_arrow.svg'}
+          ></Image>
+        }
+        rightContent="다음"
+        onPrevious={onPrevious}
+        onNext={() => onNext(gender)}
+      ></NavigationBar>
+
+      <Paragraph
+        gap="5px"
+        fontSize="35px"
+        sentences={[
+          <BoldText boldContent="성별" normalContent="을"></BoldText>,
+          '선택해주세요',
+        ]}
+      ></Paragraph>
+
+      <ButtonWrapper>
+        <Button
+          color="#ffffff"
+          mode="NORMAL_GRAY"
+          msg="남성"
+          onClick={() => setGender('남성')}
+        ></Button>
+        <Button
+          color="#ffffff"
+          mode="NORMAL_GRAY"
+          msg="여성"
+          onClick={() => setGender('여성')}
+        ></Button>
+      </ButtonWrapper>
     </Wrapper>
   );
 }
