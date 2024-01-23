06286268b3a32cb39be3ea04a42e3ee0f8a5739a
황준식 <wnstlr0908@naver.com>
2024-01-23 17:28:51 +0900

    S10P12A407-8 feat: 캐러셀 안에 이미지 넣기

--- frontend/src/components/common/SimpleSlider.jsx
+++ frontend/src/components/common/SimpleSlider.jsx
@@ -6,8 +6,21 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const ImageSlide = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 260px;
+  height: 260px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CustomDotWrapper = styled.div`
+  position: absolute;
+  bottom: 0px; /* 원하는 위치로 조절 */
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function SimpleSlider({ showImages }) {
@@ -17,14 +30,15 @@ function SimpleSlider({ showImages }) {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true, // 좌,우 버튼
+    arrows: false, // 좌,우 버튼
+    appendDots: dots => <CustomDotWrapper>{dots}</CustomDotWrapper>,
   };
 
   return (
     <Slider {...settings}>
       {showImages.map((image, id) => (
         <ImageSlide key={id}>
-          <Image src={image} width={'100px'} height={'100px'}></Image>
+          <Image src={image} width={'240px'} height={'240px'}></Image>
         </ImageSlide>
       ))}
     </Slider>
