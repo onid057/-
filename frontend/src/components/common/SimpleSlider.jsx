2e05f499d24c4bd3b044aceffede8bf5eb882782
황준식 <wnstlr0908@naver.com>
2024-01-23 13:30:24 +0900

    S10P12A407-8 fix: 캐러셀 슬라이드 컴포넌트

--- frontend/src/components/common/SimpleSlider.jsx
+++ frontend/src/components/common/SimpleSlider.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import Slider from 'react-slick';
+import Image from './Image';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
+const ImageSlide = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 function SimpleSlider({ showImages }) {
   const settings = {
@@ -16,9 +23,9 @@ function SimpleSlider({ showImages }) {
   return (
     <Slider {...settings}>
       {showImages.map((image, id) => (
-        <div key={id}>
-          <img src={image} alt={id} />
-        </div>
+        <ImageSlide key={id}>
+          <Image src={image} width={'100px'} height={'100px'}></Image>
+        </ImageSlide>
       ))}
     </Slider>
   );
