00551115560106035e90d77d0f3cd5463fecbdb1
곽희웅 <gmldnd567@naver.com>
2024-01-23 14:35:14 +0900

    fix: successCode 주석 수정

--- backend/src/main/java/com/a407/back/config/SuccessCode.java
+++ backend/src/main/java/com/a407/back/config/SuccessCode.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum SuccessCode {
-    // 조회 성공!
+    // 조회 성공!!
     SELECT_SUCCESS(200, "SELECT SUCCESS"),
-    // 삭제 성공!
+    // 삭제 성공!!
     DELETE_SUCCESS(200, "DELETE SUCCESS"),
-    // 삽입 성공!
+    // 삽입 성공!!
     INSERT_SUCCESS(201, "INSERT SUCCESS"),
-    // 수정 성공!
+    // 수정 성공!!
     UPDATE_SUCCESS(204, "UPDATE SUCCESS");
 
     private final int status;
