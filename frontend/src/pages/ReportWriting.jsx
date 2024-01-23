94092fdf0bf90319a192e055ace973ed7bc6170c
황준식 <wnstlr0908@naver.com>
2024-01-23 17:30:21 +0900

    S10P12A407-8 fix: LongInputBox.jsx 적용 및 페이지 사이즈, 크기 정렬

--- frontend/src/pages/ReportWriting.jsx
+++ frontend/src/pages/ReportWriting.jsx
@@ -3,6 +3,7 @@ import Button from '../components/common/Button';
 import styled from 'styled-components';
 import NavigationBar from '../components/common/NavigationBar';
 import ImageUploader from '../components/common/ImageUploader';
+import LongInputBox from '../components/common/LongInputBox';
 
 const ReportWritingWrapper = styled.div`
   width: 320px;
@@ -11,6 +12,7 @@ const ReportWritingWrapper = styled.div`
   padding: 0 16px;
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 20px;
   background-color: ${({ theme }) => theme.colors.primary};
   font-size: 18px;
@@ -18,23 +20,23 @@ const ReportWritingWrapper = styled.div`
   white-space: pre-wrap;
 `;
 
-const ContentWrapper = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  height: 518px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-`;
+// const ContentWrapper = styled.div`
+//   box-sizing: border-box;
+//   width: 100%;
+//   height: 518px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-between;
+//   align-items: center;
+// `;
 
-const MainContent = styled.div`
-  box-sizing: border-box;
-  width: 294px;
-  height: 264px;
-  background-color: #ffffff;
-  border-radius: 25px;
-`;
+// const MainContent = styled.div`
+//   box-sizing: border-box;
+//   width: 294px;
+//   height: 264px;
+//   background-color: #ffffff;
+//   border-radius: 25px;
+// `;
 
 function ReportWriting() {
   const fileInputRef = useRef(null);
@@ -57,32 +59,30 @@ function ReportWriting() {
         centerContent={'정기 보고서'}
         rightContent={' '}
       ></NavigationBar>
-      <ContentWrapper>
-        <ImageUploader
-          showImages={showImages}
-          setShowImages={setShowImages}
-          fileInputRef={fileInputRef}
-          Children={
-            showImages.length ? (
-              <Button
-                color="#FFFFFF"
-                mode="IMAGE_EDIT"
-                msg="수정하기"
-                onClick={handleButtonClick}
-              ></Button>
-            ) : (
-              <Button
-                color="#FFFFFF"
-                mode="IMAGE_UPLOAD"
-                msg="사진 첨부하기"
-                onClick={handleButtonClick}
-              ></Button>
-            )
-          }
-        ></ImageUploader>
-        <MainContent></MainContent>
-        <Button color="#629AF9" mode="NORMAL_BLUE" msg="작성완료"></Button>
-      </ContentWrapper>
+      <ImageUploader
+        showImages={showImages}
+        setShowImages={setShowImages}
+        fileInputRef={fileInputRef}
+        Children={
+          showImages.length ? (
+            <Button
+              color="#FFFFFF"
+              mode="IMAGE_EDIT"
+              msg="수정하기"
+              onClick={handleButtonClick}
+            ></Button>
+          ) : (
+            <Button
+              color="#FFFFFF"
+              mode="IMAGE_UPLOAD"
+              msg="사진 첨부하기"
+              onClick={handleButtonClick}
+            ></Button>
+          )
+        }
+      ></ImageUploader>
+      <LongInputBox title={''} placeholder={'hi'}></LongInputBox>
+      <Button color="#629AF9" mode="NORMAL_BLUE" msg="작성완료"></Button>
     </ReportWritingWrapper>
   );
 }
