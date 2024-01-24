02a1c8828096544dd8831be18f843b9de3ec4287
황준식 <wnstlr0908@naver.com>
2024-01-24 16:21:27 +0900

    S10P12A407-8 fix: 캐러셀 크기 및 구조 변경과 닫기 버튼 로직 추가

--- frontend/src/components/common/SimpleSlider.jsx
+++ frontend/src/components/common/SimpleSlider.jsx
@@ -5,25 +5,51 @@ import Image from './Image';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const ImageSlide = styled.div`
-  width: 260px;
-  height: 260px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const StyledSlider = styled(Slider)`
+  height: 100%;
+  .slick-list {
+    height: 100%;
+    object-fit: cover;
+    display: flex;
+    align-items: center; // 이미지가 정방향이 아닐 경우 가운데 위치
+  }
+  .slick-track {
+    display: flex;
+    align-items: center;
+  }
+  .slick-prev {
+    left: 6px;
+    z-index: 999;
+  }
+  .slick-next {
+    right: 6px;
+    z-index: 999;
+  }
 `;
 
 const CustomDotWrapper = styled.div`
-  position: absolute;
-  bottom: 0px; /* 원하는 위치로 조절 */
+  /* position: absolute;
+  bottom: -25px;
   left: 50%;
-  transform: translateX(-50%);
+  transform: translateX(-50%); */
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-function SimpleSlider({ showImages }) {
+const CustomButton = styled.button`
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-image: url('./closebutton.svg');
+  background-size: cover; /* 이미지를 버튼 영역에 맞게 크기 조절 */
+  background-position: center; /* 이미지를 가운데 정렬 */
+  background-repeat: no-repeat; /* 이미지 반복 없음 */
+  border: none; /* 테두리 제거 */
+  cursor: pointer; /* 커서를 포인터로 변경하여 클릭 가능하게 함 */
+`;
+
+function SimpleSlider({ showImages, setShowImages }) {
   const settings = {
     dots: true,
     infinite: true,
@@ -34,14 +60,30 @@ function SimpleSlider({ showImages }) {
     appendDots: dots => <CustomDotWrapper>{dots}</CustomDotWrapper>,
   };
 
+  // X 버튼 클릭 시 이미지 삭제
+  const handleDeleteImage = id => {
+    setShowImages(showImages.filter((_, index) => index !== id));
+  };
+
   return (
-    <Slider {...settings}>
+    <StyledSlider {...settings}>
       {showImages.map((image, id) => (
-        <ImageSlide key={id}>
-          <Image src={image} width={'240px'} height={'240px'}></Image>
-        </ImageSlide>
+        <div key={id}>
+          {/* <button onClick={() => handleDeleteImage(id)}>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/closebutton.svg`}
+              alt="close"
+            />
+          </button> */}
+          <Image
+            src={image}
+            width={'260px'}
+            height={'260px'}
+            onDelete={() => handleDeleteImage(id)}
+          ></Image>
+        </div>
       ))}
-    </Slider>
+    </StyledSlider>
   );
 }
 
