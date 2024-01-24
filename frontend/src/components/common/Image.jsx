5e24baf09588627b46398c75f6bef92c0951b092
황준식 <wnstlr0908@naver.com>
2024-01-24 16:19:57 +0900

    S10P12A407-8 fix: 이미지 생성 컴포넌트 수정 및 닫기 버튼 svg 파일 추가

--- frontend/src/components/common/Image.jsx
+++ frontend/src/components/common/Image.jsx
@@ -8,16 +8,33 @@ const ImageWrapper = styled.div`
   background-position: center;
   background-size: cover;
   background-image: url(${props => props.$src});
+  position: relative;
 `;
 
-function Image({ src, width, height, margin }) {
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 6px;
+  right: 6px;
+  z-index: 999;
+  border-radius: 50%;
+  width: 15px;
+  height: 15px;
+  background-image: url(${props => props.$url});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border: none;
+  cursor: pointer;
+`;
+
+function Image({ src, width, height, margin, onDelete }) {
+  const CloseUrl = `${process.env.PUBLIC_URL}/images/closebutton.svg`;
   return (
-    <ImageWrapper
-      $src={src}
-      $width={width}
-      $height={height}
-      $margin={margin}
-    ></ImageWrapper>
+    <ImageWrapper $src={src} $width={width} $height={height} $margin={margin}>
+      {onDelete ? (
+        <DeleteButton $url={CloseUrl} onClick={onDelete}></DeleteButton>
+      ) : null}
+    </ImageWrapper>
   );
 }
 
