5039a0ff7b5ac55b2bfe75c88bb3784fadda90c4
장수민 <jsm0726@naver.com>
2024-01-24 16:50:28 +0900

    fix: 메인 카테고리를 저장하기 위한 useState 훅 사용

--- frontend/src/pages/filterFunnel/MainCategory.jsx
+++ frontend/src/pages/filterFunnel/MainCategory.jsx
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
-import BoldText from '../../components/common/BoldText';
+import { useState, useEffect } from 'react';
+import { styled } from 'styled-components';
+
+import NavigationBar from '../../components/common/NavigationBar';
+import Image from '../../components/common/Image';
 import Paragraph from '../../components/common/Paragraph';
+import BoldText from '../../components/common/BoldText';
 import ProgressBar from '../../components/common/ProgressBar';
 import Button from '../../components/common/Button';
 
@@ -8,73 +12,121 @@ const Wrapper = styled.div`
   width: 320px;
   min-height: 568px;
   margin: 0 auto;
-  padding: 60px 16px;
+  padding: 0 16px;
   display: flex;
   flex-direction: column;
-  gap: 11px;
+  gap: 15px;
   background-color: ${({ theme }) => theme.colors.primary};
   font-size: 18px;
   font-weight: 300;
   white-space: pre-wrap;
 `;
 
-const TitleBox = styled.div`
-  width: 100%;
-  font-size: 35px;
-`;
-
 const ContentBox = styled.div`
-  width: 100%;
-  min-height: 300px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  gap: 15px;
-`;
-
-const InnerContentBox = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
 `;
 
-function MainCategory() {
+function MainCategory({ onPrevious, onNext, matchMainCategory }) {
+  const [mainCategory, setMainCategory] = useState('');
+  const [isOptionSelected, setIsOptionSelected] = useState({
+    '동네 동행': false,
+    '멀리 동행': false,
+    집안일: false,
+    배달: false,
+    목욕: false,
+    '반려 동물': false,
+    교육: false,
+    기타: false,
+  });
+
+  console.log(mainCategory);
+
+  useEffect(() => {
+    if (matchMainCategory) {
+      setIsOptionSelected({ ...isOptionSelected, matchMainCategory: true });
+      setMainCategory(matchMainCategory);
+    }
+  }, [matchMainCategory]);
+
   return (
     <Wrapper>
-      <TitleBox>
-        <Paragraph
-          sentences={[
-            <BoldText
-              fontSize="35px"
-              boldContent="어떤 일"
-              normalContent="을"
-            ></BoldText>,
-            '맡기고 싶으신가요?',
-          ]}
-        ></Paragraph>
-      </TitleBox>
+      <NavigationBar
+        leftContent={
+          <Image
+            width="40px"
+            height="40px"
+            margin="0 0 0 -12px"
+            src={process.env.PUBLIC_URL + '/images/left_arrow.svg'}
+          ></Image>
+        }
+        rightContent="다음"
+        onPrevious={onPrevious}
+        onNext={() => onNext(mainCategory)}
+      ></NavigationBar>
 
-      {/* ProgressBar 진척도 변경 부분 */}
+      <Paragraph
+        gap="5px"
+        fontSize="35px"
+        sentences={[
+          <BoldText boldContent="어떤 일" normalContent="을"></BoldText>,
+          '맡기고 싶으신가요?',
+        ]}
+      ></Paragraph>
+
       <ProgressBar value={17}></ProgressBar>
 
       <ContentBox>
-        <InnerContentBox>
-          <Button mode={'SMALL_WHITE'} msg={'동네 동행'}></Button>
-          <Button mode={'SMALL_WHITE'} msg={'멀리 동행'}></Button>
-        </InnerContentBox>
-        <InnerContentBox>
-          <Button mode={'SMALL_WHITE'} msg={'가사'}></Button>
-          <Button mode={'SMALL_WHITE'} msg={'배달'}></Button>
-        </InnerContentBox>
-        <InnerContentBox>
-          <Button mode={'SMALL_WHITE'} msg={'목욕'}></Button>
-          <Button mode={'SMALL_WHITE'} msg={'펫 케어'}></Button>
-        </InnerContentBox>
-        <InnerContentBox>
-          <Button mode={'SMALL_WHITE'} msg={'대행'}></Button>
-          <Button mode={'SMALL_WHITE'} msg={'간단 심부름'}></Button>
-        </InnerContentBox>
+        <Button
+          mode={isOptionSelected['동네 동행'] ? 'SMALL' : 'SMALL_WHITE'}
+          msg={'동네 동행'}
+          onClick={() => {
+            setMainCategory('동네 동행');
+            setIsOptionSelected({ ...isOptionSelected, '동네 동행': true });
+          }}
+        ></Button>
+        <Button
+          mode={'SMALL_WHITE'}
+          msg={'멀리 동행'}
+          onClick={() => setMainCategory('멀리 동행')}
+        ></Button>
+      </ContentBox>
+      <ContentBox>
+        <Button
+          mode={'SMALL_WHITE'}
+          msg={'집안일'}
+          onClick={() => setMainCategory('집안일')}
+        ></Button>
+        <Button
+          mode={'SMALL_WHITE'}
+          msg={'배달'}
+          onClick={() => setMainCategory('배달')}
+        ></Button>
+      </ContentBox>
+      <ContentBox>
+        <Button
+          mode={'SMALL_WHITE'}
+          msg={'목욕'}
+          onClick={() => setMainCategory('목욕')}
+        ></Button>
+        <Button
+          mode={'SMALL_WHITE'}
+          msg={'반려 동물'}
+          onClick={() => setMainCategory('반려 동물')}
+        ></Button>
+      </ContentBox>
+      <ContentBox>
+        <Button
+          mode={'SMALL_WHITE'}
+          msg={'교육'}
+          onClick={() => setMainCategory('교육')}
+        ></Button>
+        <Button
+          mode={'SMALL_WHITE'}
+          msg={'기타'}
+          onClick={() => setMainCategory('기타')}
+        ></Button>
       </ContentBox>
     </Wrapper>
   );
