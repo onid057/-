2e842a3b657c55411017f2a1f6136d44dc908b15
jangsumin <jsm0726@naver.com>
2024-01-25 23:57:55 +0900

    design: 조건 설정 인터페이스 디자인 수정

--- frontend/src/pages/filterFunnel/Condition.jsx
+++ frontend/src/pages/filterFunnel/Condition.jsx
@@ -1,4 +1,7 @@
-import styled from 'styled-components';
+import { styled } from 'styled-components';
+
+import NavigationBar from '../../components/common/NavigationBar';
+import Image from '../../components/common/Image';
 import BoldText from '../../components/common/BoldText';
 import Paragraph from '../../components/common/Paragraph';
 import ProgressBar from '../../components/common/ProgressBar';
@@ -8,25 +11,14 @@ const Wrapper = styled.div`
   width: 320px;
   min-height: 568px;
   margin: 0 auto;
-  padding: 60px 16px;
+  padding: 0 16px 16px 16px;
   display: flex;
   flex-direction: column;
-  gap: 11px;
+  gap: 15px;
   background-color: ${({ theme }) => theme.colors.primary};
   font-size: 18px;
   font-weight: 300;
   white-space: pre-wrap;
-`;
-
-const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-`;
-
-const TitleBox = styled.div`
-  width: 100%;
-  font-size: 35px;
 `;
 
 const ContentTitle = styled.div`
@@ -34,18 +26,16 @@ const ContentTitle = styled.div`
   width: 100%;
   display: flex;
   justify-content: flex-start;
-  font-size: 20px;
-  font-weight: light;
+  font-size: 16px;
 `;
 
 const ContentBox = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  /* justify-content: space-between; */
   justify-content: flex-start;
   align-items: center;
-  gap: 15px;
+  gap: 10px;
 `;
 
 const InnerContentBox = styled.div`
@@ -56,76 +46,82 @@ const InnerContentBox = styled.div`
 `;
 
 // 앞에서 대분류 선택에 따라서 여기 subCategoryList가 달라져야 하는데 어떻게 하는지 잘 모르겠음...
-function Description() {
+function Condition({ onPrevious, onNext }) {
   return (
     <Wrapper>
-      <TitleBox>
-        <Paragraph
-          sentences={[
-            <BoldText
-              fontSize="35px"
-              boldContent="집사의 조건"
-              normalContent="을"
-            ></BoldText>,
-            '정해 주세요',
-          ]}
-        ></Paragraph>
-      </TitleBox>
+      <NavigationBar
+        leftContent={
+          <Image
+            width="40px"
+            height="40px"
+            margin="0 0 0 -12px"
+            src={process.env.PUBLIC_URL + '/images/left_arrow.svg'}
+          ></Image>
+        }
+        rightContent="완료"
+        onPrevious={onPrevious}
+        onNext={onNext}
+      ></NavigationBar>
+
+      <Paragraph
+        gap="5px"
+        fontSize="35px"
+        sentences={[
+          <BoldText boldContent="집사의 조건" normalContent="을"></BoldText>,
+          '정해 주세요',
+        ]}
+      ></Paragraph>
 
       {/* ProgressBar 진척도 변경 부분 */}
       <ProgressBar value={100}></ProgressBar>
 
-      <ContentWrapper>
-        <ContentBox>
-          <ContentTitle>성별</ContentTitle>
-          <InnerContentBox>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'무관'}></Button>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'남성'}></Button>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'여성'}></Button>
-          </InnerContentBox>
-        </ContentBox>
+      <ContentBox>
+        <ContentTitle>성별</ContentTitle>
+        <InnerContentBox>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'무관'}></Button>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'남성'}></Button>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'여성'}></Button>
+        </InnerContentBox>
+      </ContentBox>
 
-        <ContentBox>
-          <ContentTitle>연령</ContentTitle>
-          <InnerContentBox>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'무관'}></Button>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'40대 미만'}></Button>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'40대 이상'}></Button>
-          </InnerContentBox>
-        </ContentBox>
+      <ContentBox>
+        <ContentTitle>연령</ContentTitle>
+        <InnerContentBox>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'무관'}></Button>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'40대 미만'}></Button>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'40대 이상'}></Button>
+        </InnerContentBox>
+      </ContentBox>
 
-        <ContentBox>
-          <ContentTitle>집사 등급</ContentTitle>
-          <InnerContentBox>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'무관'}></Button>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'40대 미만'}></Button>
-          </InnerContentBox>
-          <InnerContentBox>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'2'}></Button>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'3'}></Button>
-          </InnerContentBox>
-          <InnerContentBox>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'프로 집사'}></Button>
-            <Button mode={'FULL_PERCENT_WHITE'} msg={'전설 집사'}></Button>
-          </InnerContentBox>
-        </ContentBox>
+      <ContentBox>
+        <ContentTitle>집사 등급</ContentTitle>
+        <InnerContentBox>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'무관'}></Button>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'견습 집사'}></Button>
+        </InnerContentBox>
+        <InnerContentBox>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'프로 집사'}></Button>
+          <Button mode={'FULL_PERCENT_WHITE'} msg={'전설 집사'}></Button>
+        </InnerContentBox>
+      </ContentBox>
 
-        <ContentBox>
-          <ContentTitle>다이아몬드 평점</ContentTitle>
-          <InnerContentBox>
-            <Button mode={'INSIDE_IMAGE'} msg={'무관'}></Button>
-            <Button mode={'INSIDE_IMAGE'} msg={'5 이상'}></Button>
-            <Button mode={'INSIDE_IMAGE'} msg={'4 이상'}></Button>
-          </InnerContentBox>
-          <InnerContentBox>
-            <Button mode={'INSIDE_IMAGE'} msg={'3 이상'}></Button>
-            <Button mode={'INSIDE_IMAGE'} msg={'2 이상'}></Button>
-            <Button mode={'INSIDE_IMAGE'} msg={'1 이상'}></Button>
-          </InnerContentBox>
-        </ContentBox>
-      </ContentWrapper>
+      <ContentBox>
+        <ContentTitle>다이아몬드 평점</ContentTitle>
+        <InnerContentBox>
+          <Button mode={'INSIDE_IMAGE'} msg={'5 이상'}></Button>
+          <Button mode={'INSIDE_IMAGE'} msg={'4 이상'}></Button>
+        </InnerContentBox>
+        <InnerContentBox>
+          <Button mode={'INSIDE_IMAGE'} msg={'3 이상'}></Button>
+          <Button mode={'INSIDE_IMAGE'} msg={'2 이상'}></Button>
+        </InnerContentBox>
+        <InnerContentBox>
+          <Button mode={'INSIDE_IMAGE'} msg={'1 이상'}></Button>
+          <Button mode={'INSIDE_IMAGE'} msg={'0 이상'}></Button>
+        </InnerContentBox>
+      </ContentBox>
     </Wrapper>
   );
 }
 
-export default Description;
+export default Condition;
