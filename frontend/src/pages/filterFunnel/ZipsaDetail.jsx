e2b67c4917aef7588f68bceb66f4c43dbe92b385
waterlee6 <waterleee6@gmail.com>
2024-01-26 15:58:25 +0900

    S10P12A407-78 feat: 집사 상세 정보 조회 페이지

--- frontend/src/pages/filterFunnel/ZipsaDetail.jsx
+++ frontend/src/pages/filterFunnel/ZipsaDetail.jsx
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
 
-function TestLee() {
-  return <></>;
+const Wrapper = styled.div`
+  width: 320px;
+  min-height: 568px;
+  margin: 0 auto;
+  padding: 60px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  font-size: 18px;
+  font-weight: 300;
+  white-space: pre-wrap;
+`;
+
+const Profile = styled.div``;
+
+const Moveto = styled.div``;
+
+function ZipsaDetail() {
+  return (
+    <Wrapper>
+      <h1>ddd</h1>
+    </Wrapper>
+  );
 }
 
-export default TestLee;
+export default ZipsaDetail;
