25c91e3a7dd3735fb370a2c64ac5bac493afa61a
강태연 <kangty1028@naver.com>
2024-01-29 13:53:45 +0900

    feat: 인증 메세지 전송, 전화 번호 저장

---
+++ backend/src/main/java/com/a407/back/config/MessageConfig.java
@@ -0,0 +1,24 @@
+package com.a407.back.config;
+
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConfig {
+
+    @Value("${sms.api.key}")
+    private String apiKey;
+
+    @Value("${sms.api.secret}")
+    private String secret;
+
+
+    @Bean
+    public DefaultMessageService messageService(){
+        return NurigoApp.INSTANCE.initialize(apiKey,secret,"https://api.coolsms.co.kr");
+    }
+
+}
---
+++ backend/src/main/java/com/a407/back/dto/User/UserPhoneNumberRequest.java
@@ -0,0 +1,14 @@
+package com.a407.back.dto.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPhoneNumberRequest {
+
+    private String phoneNumber;
+
+}
