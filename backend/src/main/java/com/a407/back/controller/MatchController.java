c97af4bcf38c17eaa9f44bd4a298726cde19d65f
곽희웅 <gmldnd567@naver.com>
2024-01-30 14:10:15 +0900

    S10P12A407-95 fix: 불필요한 import문 제거

--- backend/src/main/java/com/a407/back/controller/MatchController.java
+++ backend/src/main/java/com/a407/back/controller/MatchController.java
@@ -2,12 +2,10 @@ package com.a407.back.controller;
 
 import com.a407.back.config.constants.SuccessCode;
 import com.a407.back.dto.util.ApiResponse;
-import com.a407.back.dto.Match.MatchCreateRequest;
 import com.a407.back.dto.Match.MatchSearchRequest;
 import com.a407.back.dto.Match.MatchSearchResponse;
 import com.a407.back.dto.Match.RoomCreateRequest;
 import com.a407.back.model.service.MatchService;
-import com.a407.back.model.service.RoomService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,8 +24,6 @@ public class MatchController {
 
     private final MatchService matchService;
 
-    private final RoomService roomService;
-
     @GetMapping("/filter")
     public ResponseEntity<ApiResponse<List<MatchSearchResponse>>> getZipsas(
         @ModelAttribute MatchSearchRequest condition) {
@@ -37,15 +33,6 @@ public class MatchController {
         ApiResponse<List<MatchSearchResponse>> response = new ApiResponse<>(
             SuccessCode.SELECT_SUCCESS, matchSearchResponses);
         return ResponseEntity.ok(response);
-    }
-
-
-    @PostMapping("/")
-    public ResponseEntity<ApiResponse<Long>> makeMatch(
-        @RequestBody MatchCreateRequest matchCreateRequest) {
-        roomService.makeMatch(matchCreateRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(
-            new ApiResponse<>(SuccessCode.UPDATE_SUCCESS, matchCreateRequest.getRoomId()));
     }
 
     @PostMapping("/choice-helper")
