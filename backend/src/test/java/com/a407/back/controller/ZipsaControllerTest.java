d902cc3adc9c3ac21a296c191643733ea6efe7ef
강태연 <kangty1028@naver.com>
2024-01-31 22:13:24 +0900

    S10P12A407-325 feat: ZipsaController 테스트 코드 작성

--- backend/src/test/java/com/a407/back/controller/ZipsaControllerTest.java
+++ backend/src/test/java/com/a407/back/controller/ZipsaControllerTest.java
@@ -4,15 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.a407.back.BackendApplication;
 import com.a407.back.domain.Grade;
+import com.a407.back.domain.Room;
 import com.a407.back.domain.User;
 import com.a407.back.domain.User.Gender;
 import com.a407.back.domain.Zipsa;
 import com.a407.back.dto.Match.RoomCreateRequest;
+import com.a407.back.dto.Zipsa.PublicRoomNotificationRequest;
 import com.a407.back.dto.Zipsa.ReportCreateRequest;
 import com.a407.back.model.service.MatchService;
+import com.a407.back.model.service.NotificationService;
 import com.a407.back.model.service.RoomService;
 import com.a407.back.model.service.UserService;
 import com.a407.back.model.service.ZipsaService;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -20,8 +24,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -43,85 +45,149 @@ class ZipsaControllerTest {
     @Autowired
     MatchService matchService;
 
+    @Autowired
+    EntityManager em;
 
-    final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    NotificationService notificationService;
 
+
+    private Long userId;
+    private Long roomId;
+    private Long zipsaId;
 
     @BeforeEach
     void beforeEach() {
-        // 여기에서 필요한 데이터들이 어떤게 있는지 생각을 해보고 값을 세팅 해주자
-        // 방의 정보와 사용자의 정보, 집사의 정보가 필요하다
-        User user = User.builder().email("user@abc.com").name("user").password("user").birth(
-                Timestamp.valueOf("2024-01-01 01:01:01")).gender(Gender.MAN).address("서울시")
+        // 사용자 생성
+        User user = User.builder().email("user@abc.com").name("user").password("user")
+            .birth(Timestamp.valueOf("2024-01-01 01:01:01")).gender(Gender.MAN).address("서울시")
             .latitude(36.5).longitude(127.5).isAdmin(false).isAffiliated(false).isBlocked(false)
             .isCertificated(false).build();
 
+        // 집사를 할 사용자 생성
         User zipsaUser = User.builder().email("zipsa@abc.com").name("zipsa").password("zipsa")
-            .birth(
-                Timestamp.valueOf("2024-01-01 01:01:01")).gender(Gender.MAN).address("서울시")
+            .birth(Timestamp.valueOf("2024-01-01 01:01:01")).gender(Gender.MAN).address("서울시")
             .latitude(36.5).longitude(127.5).isAdmin(false).isAffiliated(false).isBlocked(false)
             .isCertificated(false).build();
 
+        Grade grade = new Grade("임시 등급", 10);
+        em.persist(grade);
+        userId = userService.makeUser(user);
+        zipsaId = userService.makeUser(zipsaUser);
+        assertThat(userService.findByUserId(userId)).isEqualTo(userService.findByUserId(userId));
+        assertThat(userService.findByUserId(zipsaId)).isEqualTo(userService.findByUserId(zipsaId));
         Zipsa zipsa = Zipsa.builder().zipsaId(zipsaUser).account("계좌").description("설명")
-            .gradeId(new Grade("임시 등급", 5000)).isWorked(true).kindnessAverage(0D).skillAverage(0D)
+            .gradeId(grade).isWorked(true).kindnessAverage(0D).skillAverage(0D)
             .rewindAverage(0D).replyAverage(0D).replyCount(0).preferTag("임시 태그").build();
+        em.persist(zipsa);
+        assertThat(zipsaService.findByZipsaId(zipsaId).getDescription()).isEqualTo("설명");
 
         List<Long> list = new ArrayList<>();
         list.add(1L);
-        RoomCreateRequest roomCreateRequest = new RoomCreateRequest(1L, 1L, "제목", "1", "장소", 12,
+        RoomCreateRequest roomCreateRequest = new RoomCreateRequest(userId, 1L, "제목", "1", "장소", 12,
             Timestamp.valueOf("2024-01-01 01:01:01"), Timestamp.valueOf("2024-01-01 01:01:01"),
             Timestamp.valueOf("2024-01-01 01:01:01"), 15000, list);
-
-        matchService.makeFilterRoom(roomCreateRequest);
-
-        userService.save(user);
-        userService.save(zipsaUser);
+        roomId = matchService.makeFilterRoom(roomCreateRequest);
     }
 
     @Test
-    @DisplayName("정기 보고 추가 + 조회")
-    void reportAdd() {
-        List<Long> list = new ArrayList<>();
-        list.add(1L);
-        RoomCreateRequest roomCreateRequest = new RoomCreateRequest(1L, 1L, "제목", "1", "장소", 12,
-            Timestamp.valueOf("2024-01-01 01:01:01"), Timestamp.valueOf("2024-01-01 01:01:01"),
-            Timestamp.valueOf("2024-01-01 01:01:01"), 15000, list);
+    @DisplayName("정기 보고 추가")
+    void makeReport() {
+        ReportCreateRequest reportCreateRequest = new ReportCreateRequest(roomId,
+            "https://www.flaticon.com/kr/free-icon-font/phone-call_5070407", "내용");
+        zipsaService.makeReport(reportCreateRequest);
+        ReportCreateRequest reportCreateRequestTwo = new ReportCreateRequest(roomId,
+            "https://www.flaticon.com/kr/free-icon-font/phone-call_5070407", "내용2");
+        zipsaService.makeReport(reportCreateRequestTwo);
+        assertThat(zipsaService.findReportByRoomIdList(roomId).getList()).hasSize(2);
+        assertThat(zipsaService.findReportByRoomIdList(roomId).getList().get(0)
+            .getProcessContent()).isEqualTo("내용");
+    }
 
-        Long roomId=matchService.makeFilterRoom(roomCreateRequest);
-
-        ReportCreateRequest reportCreateRequest = new ReportCreateRequest(
-            roomId, "https://www.flaticon.com/kr/free-icon-font/phone-call_5070407", "내용");
-        zipsaService.saveReport(reportCreateRequest);
-        assertThat(
-            zipsaService.reportFindByRoomId(roomId).getList().get(0).getProcessContent()).isEqualTo(
-            "내용");
-        logger.info("생성, 조회 성공");
+    @Test
+    @DisplayName("정기 보고 조회")
+    void findReportByRoomIdList() {
+        ReportCreateRequest reportCreateRequest = new ReportCreateRequest(roomId,
+            "https://www.flaticon.com/kr/free-icon-font/phone-call_5070407", "내용");
+        zipsaService.makeReport(reportCreateRequest);
+        ReportCreateRequest reportCreateRequestTwo = new ReportCreateRequest(roomId,
+            "https://www.flaticon.com/kr/free-icon-font/phone-call_5070407", "내용2");
+        zipsaService.makeReport(reportCreateRequestTwo);
+        assertThat(zipsaService.findReportByRoomIdList(roomId).getList()).hasSize(2);
     }
 
     @Test
     @DisplayName("집사 상세 정보 조회")
-    void zipsaDetailInfo() {
-        // 집사를 추가하는 api는 아직 작성되지 않아서 dump로 제공된 집사의 정보를 사용
-        assertThat(zipsaService.zipsaAndReviewFindByZipsaId(3L).getName()).isEqualTo("user3");
-        logger.info("집사 상세 정보(리뷰 포함)");
+    void findZipsaAndReviewFindByZipsaId() {
+        ReportCreateRequest reportCreateRequest = new ReportCreateRequest(roomId,
+            "https://www.flaticon.com/kr/free-icon-font/phone-call_5070407", "내용");
+        zipsaService.makeReport(reportCreateRequest);
+        ReportCreateRequest reportCreateRequestTwo = new ReportCreateRequest(roomId,
+            "https://www.flaticon.com/kr/free-icon-font/phone-call_5070407", "내용2");
+        zipsaService.makeReport(reportCreateRequestTwo);
+        assertThat(zipsaService.findReportByRoomIdList(roomId).getList()).hasSize(2);
+        assertThat(zipsaService.findZipsaAndReviewFindByZipsaId(zipsaId).getName()).isEqualTo(
+            "zipsa");
+    }
+
+    @Test
+    @DisplayName("집사 예약 내역 확인")
+    void getUserRecordList() {
+        Zipsa zipsa = zipsaService.findByZipsaId(
+            zipsaService.findByZipsaId(zipsaId).getZipsaId().getUserId());
+        roomService.changeRoomZipsa(zipsa, roomId);
+        em.flush();
+        em.clear();
+
+        assertThat(zipsaService.getZipsaReservationList(zipsaId).getList()).isEmpty();
+
+        roomService.changeRoomStatus(roomId, "BEFORE");
+        em.flush();
+        em.clear();
+
+        assertThat(zipsaService.getZipsaReservationList(zipsaId).getList()).hasSize(1);
+
+        assertThat(
+            roomService.findByRoomId(roomId).getZipsaId().getZipsaId().getUserId()).isEqualTo(
+            zipsa.getZipsaId().getUserId());
     }
 
     @Test
     @DisplayName("집사 이용 내역 확인")
-    void getUserRecords() {
+    void getZipsaReservationList() {
+        Zipsa zipsa = zipsaService.findByZipsaId(
+            zipsaService.findByZipsaId(zipsaId).getZipsaId().getUserId());
+        roomService.changeRoomZipsa(zipsa, roomId);
+        em.flush();
+        em.clear();
 
+        assertThat(zipsaService.getUserRecordList(zipsaId).getList()).isEmpty();
+
+        roomService.changeRoomStatus(roomId, "END");
+        em.flush();
+        em.clear();
+
+        assertThat(zipsaService.getUserRecordList(zipsaId).getList()).hasSize(1);
     }
 
-    @Test
-    @DisplayName("집사 예약, 진행 내역 확인")
-    void getZipsaReservations() {
-
-    }
 
     @Test
     @DisplayName("알림")
     void makePublicRoomNotification() {
+        roomService.changeRoomZipsa(zipsaService.findByZipsaId(zipsaId), roomId);
+        em.flush();
+        em.clear();
 
+        Room room = em.find(Room.class, roomId);
+        assertThat(room.getNotificationCount()).isEqualTo(1);
+        PublicRoomNotificationRequest publicRoomNotificationRequest = new PublicRoomNotificationRequest(
+            roomId, zipsaId);
+
+        zipsaService.makePublicRoomNotification(publicRoomNotificationRequest);
+        em.flush();
+        em.clear();
+        room = em.find(Room.class, roomId);
+        assertThat(room.getNotificationCount()).isEqualTo(2);
     }
 
 
