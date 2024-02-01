2e9545131592e7e579295f70d6fdc6aa55619699
강태연 <kangty1028@naver.com>
2024-02-01 14:32:27 +0900

    merge: refactoring to back

--- backend/src/main/java/com/a407/back/controller/AssociationController.java
+++ backend/src/main/java/com/a407/back/controller/AssociationController.java
@@ -1,14 +1,16 @@
 package com.a407.back.controller;
 
 import com.a407.back.config.constants.SuccessCode;
-import com.a407.back.dto.user.UserAssociationResponse;
 import com.a407.back.dto.association.AssociationAdditionCodeResponse;
+import com.a407.back.dto.user.UserAssociationResponse;
 import com.a407.back.dto.util.ApiResponse;
 import com.a407.back.model.service.AssociationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,48 +26,54 @@ public class AssociationController {
     private final AssociationService associationService;
 
     @PostMapping
-    public ApiResponse<String> makeAssociation() {
+    public ResponseEntity<ApiResponse<String>> makeAssociation() {
         // 스프링 시큐리티 도입 전 임시 값
         associationService.makeAssociation(7L);
-        return new ApiResponse<>(SuccessCode.INSERT_SUCCESS, "연동 계정 생성 성공");
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponse<>(SuccessCode.INSERT_SUCCESS, "연동 계정 생성 성공"));
     }
 
 
     @GetMapping
-    public ApiResponse<List<UserAssociationResponse>> getAssociationUserList() {
+    public ResponseEntity<ApiResponse<List<UserAssociationResponse>>> getAssociationUserList() {
         // 스프링 시큐리티 도입 전 임시 값
-        return new ApiResponse<>(SuccessCode.SELECT_SUCCESS,
-            associationService.getAssociationUserList(1L));
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new ApiResponse<>(SuccessCode.SELECT_SUCCESS,
+                associationService.getAssociationUserList(1L)));
     }
 
     @DeleteMapping
-    public ApiResponse<String> deleteAssociation() {
+    public ResponseEntity<ApiResponse<String>> deleteAssociation() {
         // 스프링 시큐리티 도입 전 임시 값
         associationService.deleteAssociation(7L);
-        return new ApiResponse<>(SuccessCode.DELETE_SUCCESS, "연동 계정 삭제 성공");
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new ApiResponse<>(SuccessCode.DELETE_SUCCESS, "연동 계정 삭제 성공"));
     }
 
 
     @PostMapping("/addition")
-    public ApiResponse<AssociationAdditionCodeResponse> makeAdditionCode()
+    public ResponseEntity<ApiResponse<AssociationAdditionCodeResponse>> makeAdditionCode()
         throws NoSuchAlgorithmException, JsonProcessingException {
         // 스프링 시큐리티 도입 전 임시 값
-        return new ApiResponse<>(SuccessCode.INSERT_SUCCESS,
-            associationService.makeAdditionCode(7L, "test@test.com", 6L));
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponse<>(SuccessCode.INSERT_SUCCESS,
+                associationService.makeAdditionCode(7L, "test@test.com", 6L)));
     }
 
     @PostMapping("/participate")
-    public ApiResponse<String> changeAddition() {
+    public ResponseEntity<ApiResponse<String>> changeAddition() {
         // 스프링 시큐리티 도입 전 임시 값
         associationService.changeAssociation(8L, "66513330");
-        return new ApiResponse<>(SuccessCode.INSERT_SUCCESS, "연동 계정 참가 성공");
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponse<>(SuccessCode.INSERT_SUCCESS, "연동 계정 참가 성공"));
     }
 
     @PatchMapping("/representative")
-    public ApiResponse<String> changeAssociationRepresentative() {
+    public ResponseEntity<ApiResponse<String>> changeAssociationRepresentative() {
         // 스프링 시큐리티 도입 전 임시 값
         associationService.changeAssociationRepresentative(8L, 8L);
-        return new ApiResponse<>(SuccessCode.UPDATE_SUCCESS, "연동 계정 대표 변경 성공");
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new ApiResponse<>(SuccessCode.UPDATE_SUCCESS, "연동 계정 대표 변경 성공"));
     }
 
 
