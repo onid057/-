54e66dd67cc01dfd927be27026fa6e1de8188408
강태연 <kangty1028@naver.com>
2024-02-02 08:39:19 +0900

    merge: 153 to back

---
+++ backend/src/main/java/com/a407/back/dto/association/AssociationChangeRepresentativeRequest.java
@@ -0,0 +1,15 @@
+package com.a407.back.dto.association;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssociationChangeRepresentativeRequest {
+
+    Long representativeId;
+    Long userId;
+
+}
