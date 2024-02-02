5503837c3323055a54ed27f4a7919e025c01b094
곽희웅 <gmldnd567@naver.com>
2024-02-02 14:14:02 +0900

    fix: corsConfig

--- backend/src/main/java/com/a407/back/config/CorsConfig.java
+++ backend/src/main/java/com/a407/back/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*")
+        registry.addMapping("/**").allowedOrigins("http://127.0.0.1:3000").allowedMethods("GET, POST, PUT, DELETE, PATCH")
             .allowedHeaders("*").maxAge(3000);
     }
 }
