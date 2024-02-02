22c49142a9eeacfbea9d734c3f31462b5be7faf4
이수민 <eelsm6@naver.com>
2024-02-02 16:48:00 +0900

    S10P12A407-44 design: LongInputBox 컴포넌트 디자인 변경

--- frontend/src/components/common/LongInputBox.jsx
+++ frontend/src/components/common/LongInputBox.jsx
@@ -6,16 +6,7 @@ const LongInputBoxWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 5px;
-`;
-
-const InsideWrapper = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  margin-bottom: 21px;
+  gap: 10px;
 `;
 
 const TitleBox = styled.div`
@@ -61,20 +52,18 @@ function LongInputBox({ title, placeholder, value, onChange }) {
 
   return (
     <LongInputBoxWrapper>
-      <InsideWrapper>
-        {/* 여기에 상세 input의 제목을 입력합니다 */}
-        {title && <TitleBox>{title}</TitleBox>}
-        <ContentBox>
-          <TextAreaInput
-            // 여기에 placeholder를 입력합니다(개행문자 &#10; 사용하기)
-            // "ex)&#10;아파트 정문에 있는 공원 정문에서 만나요. 저는 벤치에 앉아있을 테니 먼저 말을 걸어주세요."
-            placeholder={placeholder}
-            type="text"
-            value={value}
-            onChange={onChange}
-          ></TextAreaInput>
-        </ContentBox>
-      </InsideWrapper>
+      {/* 여기에 상세 input의 제목을 입력합니다 */}
+      {title && <TitleBox>{title}</TitleBox>}
+      <ContentBox>
+        <TextAreaInput
+          // 여기에 placeholder를 입력합니다(개행문자 &#10; 사용하기)
+          // "ex)&#10;아파트 정문에 있는 공원 정문에서 만나요. 저는 벤치에 앉아있을 테니 먼저 말을 걸어주세요."
+          placeholder={placeholder}
+          type="text"
+          value={value}
+          onChange={onChange}
+        ></TextAreaInput>
+      </ContentBox>
     </LongInputBoxWrapper>
   );
 }
