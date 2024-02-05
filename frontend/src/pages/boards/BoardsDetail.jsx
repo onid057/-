db01d6d574c9041b1113d2b0b41d39c2ea778fa0
이수민 <eelsm6@naver.com>
2024-02-05 17:36:35 +0900

    S10P12A407-44 feat: 게시판 상세 조회 페이지

---
+++ frontend/src/pages/boards/BoardsDetail.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import NavigationBar from '../../components/common/NavigationBar';
+import Image from '../../components/common/Image';
+
+const Wrapper = styled.div`
+  width: 320px;
+  min-height: 568px;
+  margin: 0 auto;
+  padding: 0px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  font-size: 14px;
+  font-weight: 300;
+  white-space: pre-wrap;
+`;
+
+function BoardsDetail() {
+  return (
+    <Wrapper>
+      <NavigationBar
+        leftContent={
+          <Image
+            src={`${process.env.PUBLIC_URL}/images/keyboard_arrow_left.svg`}
+            width={'40px'}
+            height={'40px'}
+            margin={'0 0 0 -12px'}
+          ></Image>
+        }
+      ></NavigationBar>
+    </Wrapper>
+  );
+}
+
+export default BoardsDetail;
