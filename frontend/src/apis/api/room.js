7b5d22d0f3fb31336897780403c3fefef1a0bc0f
황준식 <wnstlr0908@naver.com>
2024-02-05 17:32:23 +0900

    feat: 공개방 API 통신 js 파일 생성

---
+++ frontend/src/apis/api/room.js
@@ -0,0 +1,39 @@
+import axios from '../utils/instance';
+
+// createRoomFunnel에서 얻은 정보를 토대로 공개방 생성
+const makeRoom = async (
+  userId,
+  subCategoryId,
+  title,
+  content,
+  place,
+  estimateDuration,
+  roomCreatedAt,
+  expectationStartedAt,
+  expectationEndedAt,
+  expectationPay,
+) => {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: '/rooms',
+      data: {
+        userId,
+        subCategoryId,
+        title,
+        content,
+        place,
+        estimateDuration,
+        roomCreatedAt,
+        expectationStartedAt,
+        expectationEndedAt,
+        expectationPay,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { makeRoom };
