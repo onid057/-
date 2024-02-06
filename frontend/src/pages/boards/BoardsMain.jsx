8eea1fc9ef8104de107be304c428303a0ddda5af
이수민 <eelsm6@naver.com>
2024-02-06 17:34:12 +0900

    S10P12A407-44 feat: 게시판 메인 페이지

--- frontend/src/pages/boards/BoardsMain.jsx
+++ frontend/src/pages/boards/BoardsMain.jsx
@@ -4,6 +4,7 @@ import Notice from '../../components/common/Notice';
 import Image from '../../components/common/Image';
 import BoldText from '../../components/common/BoldText';
 import BoardsTags from '../../components/boards/BoardsTags';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
   width: 320px;
@@ -20,9 +21,10 @@ const Wrapper = styled.div`
 
 // 가로스크롤 구현 위한 div
 const TagWrapper = styled.div`
-  width: 500px;
-  margin-bottom: 20px;
+  width: 100%;
+  height: 30px;
   display: flex;
+  align-content: center;
   gap: 8px;
   overflow: scroll;
   /* background: #f0d9ff; */
@@ -48,26 +50,81 @@ const CreateNewBox = styled.div`
   font-weight: 300;
 `;
 
-const SmallText = styled.div`
+const SelectedTag = styled.div`
+  width: 100%;
+  height: auto;
+  white-space: pre-wrap;
+  font-size: 16px;
+  font-weight: 400;
+  color: #3f3f3f;
+`;
+
+const Article = styled.div`
+  cursor: pointer;
+  width: 100%;
+  height: auto;
+  padding: 20px 13px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  border-radius: 25px;
+  background-color: white;
+`;
+
+const InsideTags = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 5px;
+`;
+
+const ArticleTitle = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const ArticleText = styled.div`
   font-size: 15px;
-  font-weight: 200;
+  font-weight: 300;
   text-align: end;
 `;
 
 // 전체 리스트 조회 API
 // API 나오면 더 봐야 할듯여
 
+// 게시판 태그 목록과 길이
+const allTags = [
+  '전체',
+  '맛집 추천',
+  '동네 소식',
+  '집사 후기',
+  '모임 모집',
+  '생활정보',
+  '일상',
+  '공공소식',
+];
+const tagLength = allTags.length;
+
 function BoardsMain() {
-  // 게시판 태그 목록
-  const boradTags = [
-    '맛집 추천',
-    '동네 소식',
-    '집사 후기',
-    '모임 모집',
-    '생활정보',
-    '일상',
-    '공공소식',
-  ];
+  // useState 사용 위해 게시판 태그 길이만큼 빈 리스트 만들기
+  const tagCheckList = Array.from({ length: tagLength }, () => false);
+
+  // tagCheckList의 상태를 관리할 useState 함수
+  const [isSelected, setIsSelected] = useState(tagCheckList);
+
+  // 누르면 check값이 토글되는 함수
+  const toggleSelected = idx => {
+    if (idx === 0 && isSelected[0] === false) {
+      setIsSelected(Array.from({ length: tagLength }, () => true));
+    } else if (idx === 0 && isSelected[0] === true) {
+      setIsSelected(Array.from({ length: tagLength }, () => false));
+    } else {
+      setIsSelected(array => [
+        ...array.slice(0, idx),
+        !array[idx],
+        ...array.slice(idx + 1),
+      ]);
+    }
+  };
 
   return (
     <Wrapper>
@@ -99,28 +156,52 @@ function BoardsMain() {
       </CreateNewBox>
 
       <TagWrapper>
-        {boradTags.map((tag, idx) => (
-          <BoardsTags key={idx} mode={'LARGE'} tagName={tag}></BoardsTags>
+        {allTags.map((tag, idx) => (
+          <BoardsTags
+            key={idx}
+            mode={isSelected[idx] ? 'LARGE_SELECTED' : 'LARGE'}
+            tagname={tag}
+            onClick={() => toggleSelected(idx)}
+          ></BoardsTags>
         ))}
       </TagWrapper>
 
-      {/* API 받아서 map 돌기 */}
-      <Notice
-        upper={[
-          <boradTags mode={'SMALL'} tagName={'집사 후기'}></boradTags>,
-          <BoldText
-            fontSize={'18px'}
-            boldContent={'이 서비스 진짜 짱이예요'}
-            normalContent={null}
-          ></BoldText>,
-        ]}
-        lower={['김세리', '1일전', '조회수']}
-        // 해당 게시물의 상세 페이지로 이동
-        nextPage={''}
-        padding={'20px 12px'}
-      ></Notice>
+      {/* 태그가 선택되면 나오는 div */}
+      <SelectedTag>
+        {isSelected[0] ? <span># 전체 </span> : ''}
+        {allTags.map((tag, idx) =>
+          !isSelected[0] && isSelected[idx] ? <span>#{tag} </span> : '',
+        )}
+      </SelectedTag>
 
-      <Notice></Notice>
+      {/* API 받아서 map 돌기 */}
+      <Article>
+        <InsideTags>
+          <BoardsTags mode={'SMALL'} tagname={'집사 후기'}></BoardsTags>
+          <BoardsTags mode={'SMALL'} tagname={'동네 소식'}></BoardsTags>
+          <BoardsTags mode={'SMALL'} tagname={'집사 추천'}></BoardsTags>
+        </InsideTags>
+        <ArticleTitle>이 서비스 진짜 짱이예요~!</ArticleTitle>
+        <ArticleText>김세리 1일전 조회수</ArticleText>
+      </Article>
+
+      <Article>
+        <InsideTags>
+          <BoardsTags mode={'SMALL'} tagname={'모임 모집'}></BoardsTags>
+          <BoardsTags mode={'SMALL'} tagname={'생활정보'}></BoardsTags>
+        </InsideTags>
+        <ArticleTitle>이 서비스 진짜 짱이예요~!</ArticleTitle>
+        <ArticleText>김세리 1일전 조회수</ArticleText>
+      </Article>
+
+      <Article>
+        <InsideTags>
+          <BoardsTags mode={'SMALL'} tagname={'일상'}></BoardsTags>
+          <BoardsTags mode={'SMALL'} tagname={'공공소식'}></BoardsTags>
+        </InsideTags>
+        <ArticleTitle>이 서비스 진짜 짱이예요~!</ArticleTitle>
+        <ArticleText>김세리 1일전 조회수</ArticleText>
+      </Article>
     </Wrapper>
   );
 }
