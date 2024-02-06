7cff0d51e83c32271ebe881760c5ce1496e1ddcc
이수민 <eelsm6@naver.com>
2024-02-06 17:44:03 +0900

    S10P12A407-44 feat: SimpleProfile에 사용되는 수정, 삭제 버튼 컴포넌트

---
+++ frontend/src/components/common/UpdateDeleteButton.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import NavigationBar from '../../components/common/NavigationBar';
+import Image from '../../components/common/Image';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 3px 0px;
+  display: flex;
+  justify-content: start;
+  gap: 8px;
+  font-size: 14px;
+  font-weight: 300;
+`;
+
+const UpdateButton = styled.button`
+  width: 51px;
+  height: 23px;
+  border-radius: 5px;
+  background-color: #7d7d7d;
+  color: white;
+`;
+const DeleteButton = styled.button`
+  width: 51px;
+  height: 23px;
+  border-radius: 5px;
+  background-color: #7d7d7d;
+  color: white;
+`;
+
+function UpdateDeleteButton({ updateButton, deleteButton, onClick }) {
+  return (
+    <Wrapper>
+      {!updateButton ? '' : <UpdateButton onClick={onClick}>수정</UpdateButton>}
+      {!deleteButton ? '' : <DeleteButton onClick={onClick}>삭제</DeleteButton>}
+    </Wrapper>
+  );
+}
+
+export default UpdateDeleteButton;
