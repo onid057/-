482db4fefa523f4aaa6195398dfabb86d9b929c3
jangsumin <jsm0726@naver.com>
2024-02-07 00:58:37 +0900

    fix: 클러스터에 해당하는 집사 정보만 fetching

--- frontend/src/components/common/BottomSheet.jsx
+++ frontend/src/components/common/BottomSheet.jsx
@@ -3,15 +3,14 @@ import Image from './Image';
 import Button from './Button';
 import GradeBadge from './GradeBadge';
 import { getZipsaListFromMap } from '../../apis/api/map';
-import { forwardRef, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { forwardRef, useState, useEffect } from 'react';
 
 const Wrapper = styled.div`
   position: absolute;
   left: 0;
   bottom: 0;
   width: 320px;
-  height: ${props => (props.isOpen ? '300px' : '0')};
+  height: ${props => (props.$isOpen ? '300px' : '0')};
   overflow: hidden;
   transition: height 1s ease;
   display: flex;
@@ -74,100 +73,62 @@ const Text = styled.div`
   font-size: 14px;
 `;
 
-const data = [
-  {
-    name: '곽희웅',
-    gender: 'MAN',
-    gradeName: 'APPRENTICE',
-    description: '열심히 할까요',
-    preferTag: '동네 동행,멀리 동행,배달,반려 동물',
-    zipsaId: 7,
-  },
-  {
-    name: '김세리',
-    gender: 'MAN',
-    gradeName: 'APPRENTICE',
-    description: '잘 하겠습니다',
-    preferTag: '동네 동행,멀리 동행,배달,반려 동물',
-    zipsaId: 8,
-  },
-  {
-    name: '이수민',
-    gender: 'MAN',
-    gradeName: 'APPRENTICE',
-    description: '어떻게든 할게요',
-    preferTag: '동네 동행,멀리 동행,배달,반려 동물',
-    zipsaId: 9,
-  },
-  {
-    name: '황준식',
-    gender: 'MAN',
-    gradeName: 'APPRENTICE',
-    description: '잘 합니다',
-    preferTag: '동네 동행,멀리 동행,배달,반려 동물',
-    zipsaId: 10,
-  },
-];
-
 // isOpen은 bottom sheet 자체가 열렸는지 확인하는 boolean prop
 // isDetailOpen은 bottom sheet 내에서 상세 정보가 열렸는지 확인하는 boolean prop
-const BottomSheet = forwardRef(({ isOpen, onClick, ...rest }, ref) => {
-  let lat, lng;
-  if (rest) {
-    lat = rest.targetCluster?.lat;
-    lng = rest.targetCluster?.lng;
-  }
+const BottomSheet = forwardRef(
+  ({ isOpen, isDetailOpen, setIsDetailOpen, onClick, targetCluster }, ref) => {
+    const [zipsaList, setZipsaList] = useState([]);
+    const [targetZipsa, setTargetZipsa] = useState({}); // 현재 선택된 집사 한 명
 
-  // const { data, isPending, error } = useQuery({
-  //   queryKey: 'zipsaListFromMap',
-  //   queryFn: () => getZipsaListFromMap(lat, lng),
-  //   enabled: !!lat && !!lng,
-  // });
+    useEffect(() => {
+      if (targetCluster)
+        getZipsaListFromMap(targetCluster.lat, targetCluster.lng).then(
+          response => setZipsaList(response),
+        );
+    }, [targetCluster]);
 
-  const [targetZipsa, setTargetZipsa] = useState({}); // 현재 선택된 집사 한 명
-  const [isDetailOpen, setIsDetailOpen] = useState(false); // 초기에 닫혀 있음
+    return (
+      <Wrapper $isOpen={isOpen} ref={ref}>
+        <Header>
+          <>{isDetailOpen ? `${targetZipsa.name} 집사` : '집사 목록'}</>
+          <Image
+            src={`${process.env.PUBLIC_URL}/images/x.svg`}
+            width="18px"
+            height="18px"
+            onClick={onClick}
+          />
+        </Header>
 
-  return (
-    <Wrapper isOpen={isOpen} ref={ref}>
-      <Header>
-        <>{isDetailOpen ? `${targetZipsa.name} 집사` : '집사 목록'}</>
-        <Image
-          src={`${process.env.PUBLIC_URL}/images/x.svg`}
-          width="18px"
-          height="18px"
-          onClick={onClick}
-        />
-      </Header>
-
-      {!isDetailOpen ? (
-        data.map((zipsa, index) => (
-          <Name
-            key={`${zipsa.name}-${index}`}
-            onClick={() => {
-              setIsDetailOpen(true);
-              setTargetZipsa(zipsa);
-            }}
-          >
-            {zipsa.name + ' 집사'}
-          </Name>
-        ))
-      ) : (
-        <Detail>
-          <BadgeWrapper>
-            <GenderBadge>{targetZipsa.gender}</GenderBadge>
-            <GradeBadge grade={targetZipsa.gradeName}></GradeBadge>
-          </BadgeWrapper>
-          <Text>
-            {targetZipsa.preferTag.split(',').map((tag, index) => (
-              <span key={index}>{`#${tag} `}</span>
-            ))}
-          </Text>
-          <Text>{targetZipsa.description}</Text>
-          <Button mode="THIN_GRAY">집사에게 제안하기</Button>
-        </Detail>
-      )}
-    </Wrapper>
-  );
-});
+        {!isDetailOpen ? (
+          zipsaList.map((zipsa, index) => (
+            <Name
+              key={`${zipsa.name}-${index}`}
+              onClick={() => {
+                setIsDetailOpen(true);
+                setTargetZipsa(zipsa);
+              }}
+            >
+              {zipsa.name + ' 집사'}
+            </Name>
+          ))
+        ) : (
+          <Detail>
+            <BadgeWrapper>
+              <GenderBadge>{targetZipsa.gender}</GenderBadge>
+              <GradeBadge grade={targetZipsa.gradeName}></GradeBadge>
+            </BadgeWrapper>
+            <Text>
+              {targetZipsa.preferTag.split(',').map((tag, index) => (
+                <span key={index}>{`#${tag} `}</span>
+              ))}
+            </Text>
+            <Text>{targetZipsa.description}</Text>
+            <Button mode="THIN_GRAY">집사에게 제안하기</Button>
+          </Detail>
+        )}
+      </Wrapper>
+    );
+  },
+);
 
 export default BottomSheet;
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -7,7 +7,6 @@ import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import BottomSheet from '../components/common/BottomSheet';
 import { getZipsaPositionWithinTwoKilos } from '../apis/api/map';
-import { useQuery } from '@tanstack/react-query';
 
 const Wrapper = styled.div`
   position: relative;
@@ -23,25 +22,18 @@ const Wrapper = styled.div`
   white-space: pre-wrap;
 `;
 
-const data = [
-  { lat: 37.510389, lng: 127.044645 },
-  { lat: 37.506320759000715, lng: 127.05368251210247 },
-  { lat: 37.506320759000715, lng: 127.05368251210247 },
-  { lat: 37.494612, lng: 127.063642 },
-];
-
 // Map 컴포넌트를 띄울 때, lat값과 lng값이 입력되어야 함.
 function Map() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDetailOpen, setIsDetailOpen] = useState(false); // 상세 정보
+  const [positions, setPositions] = useState([]);
   const [targetCluster, setTargetCluster] = useState();
   const modalRef = useRef(null);
 
-  // 현재 API 동작하지 않음
   // 중심 좌표 기준으로 2km 이내의 집사들의 lat, lng 값을 받아옴
-  // const { data, isPending, error } = useQuery({
-  //   queryKey: ['zipsaPositionWithinTwoKilos'],
-  //   queryFn: () => getZipsaPositionWithinTwoKilos(2),
-  // });
+  useEffect(() => {
+    getZipsaPositionWithinTwoKilos(2).then(response => setPositions(response));
+  }, []);
 
   // bottom sheet 영역 이외의 부분을 클릭 시 모달 isOpen 변경
   useEffect(() => {
@@ -61,6 +53,7 @@ function Map() {
     const { Ma, La } = cluster.getCenter(); // Ma: 위도, La: 경도
     setTargetCluster({ lat: Ma, lng: La });
     setIsOpen(true);
+    setIsDetailOpen(false);
   };
 
   return (
@@ -95,7 +88,7 @@ function Map() {
           disableClickZoom={true}
           onClusterclick={onClusterclick}
         >
-          {data.map((pos, index) => (
+          {positions.map((pos, index) => (
             <MapMarker
               key={`${pos.lat}-${pos.lng}-${index}`}
               position={{
@@ -109,8 +102,12 @@ function Map() {
 
       <BottomSheet
         isOpen={isOpen}
+        isDetailOpen={isDetailOpen}
+        setIsDetailOpen={setIsDetailOpen}
         ref={modalRef}
-        onClick={() => setIsOpen(false)}
+        onClick={() => {
+          setIsOpen(false);
+        }}
         targetCluster={targetCluster}
       ></BottomSheet>
     </Wrapper>
