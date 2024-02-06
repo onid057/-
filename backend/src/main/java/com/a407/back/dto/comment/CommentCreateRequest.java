e784699c979ce395325bda6b166ae6c0da3b727c
곽희웅 <gmldnd567@naver.com>
2024-02-06 17:21:11 +0900

    S10P12A407-785 feat: comment 기능 추가

---
+++ backend/src/main/java/com/a407/back/dto/comment/CommentCreateRequest.java
@@ -0,0 +1,16 @@
+package com.a407.back.dto.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentCreateRequest {
+
+    private Long boardId;
+    private Long userId;
+    private String content;
+
+}
---
+++ backend/src/main/java/com/a407/back/dto/util/CommentListDto.java
@@ -0,0 +1,15 @@
+package com.a407.back.dto.util;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentListDto {
+    private String userName;
+    private String content;
+    private Timestamp updatedAt;
+}
