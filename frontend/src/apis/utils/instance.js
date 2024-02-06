ea5879a4f028366ed6d353d080b600ea6bddd789
황준식 <wnstlr0908@naver.com>
2024-02-06 17:20:03 +0900

    fix: API 통신 오류 해결

--- frontend/src/apis/utils/instance.js
+++ frontend/src/apis/utils/instance.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 // axios 인스턴스 생성
 // baseURL은 서버의 기본 URL
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
+  baseURL: 'https://i10a407.p.ssafy.io/api',
 });
 
 export default instance;
