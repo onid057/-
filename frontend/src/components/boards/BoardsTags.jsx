e6ddcbe8f86fa0ab53e8c23064606883ddb96d6f
이수민 <eelsm6@naver.com>
2024-02-06 17:39:07 +0900

    S10P12A407-44 design: 게시판 태그 컴포넌트

--- frontend/src/components/boards/BoardsTags.jsx
+++ frontend/src/components/boards/BoardsTags.jsx
@@ -1,36 +1,59 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  cursor: ${props => (props.$cursor ? props.$cursor : 'none')};
   width: ${props => (props.$width ? props.$width : '91px')};
   height: ${props => (props.$height ? props.$height : '27px')};
   padding: ${props => (props.$padding ? props.$padding : '3px 2px')};
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #dcf0f5;
+  background-color: ${props =>
+    props.$backgroundColor ? props.$backgroundColor : '#dcf0f5'};
   border-radius: ${props =>
     props.$borderRadius ? props.$borderRadius : '5px'};
+  white-space: nowrap;
   border: ${props => (props.$border ? props.$border : 'none')};
   font-size: ${props => (props.$fontSize ? props.$fontSize : '15px')};
   font-weight: ${props => (props.$fontWeight ? props.$fontWeight : '500')};
+  color: ${props => (props.color ? props.color : '#000000')};
 `;
 
-function BoardsTags({ mode, tagName }) {
+function BoardsTags({ mode, tagname, onClick }) {
   switch (mode) {
     case 'LARGE':
       return (
         <Wrapper
-          $width={'114px'}
+          $cursor={'pointer'}
+          $width={'95px'}
           $height={'27px'}
           $padding={'5px 15px'}
           $borderRadius={'25px'}
           $border={'solid 0.5px #1c2064'}
+          onClick={onClick}
         >
-          {tagName}
+          {tagname}
+        </Wrapper>
+      );
+    case 'LARGE_SELECTED':
+      return (
+        <Wrapper
+          $cursor={'pointer'}
+          $width={'114px'}
+          $height={'27px'}
+          $padding={'5px 15px'}
+          $backgroundColor={'#629AF9'}
+          $borderRadius={'25px'}
+          $border={'solid 0.5px #629AF9'}
+          $fontWeight={'400'}
+          color={'#FFFFFF'}
+          onClick={onClick}
+        >
+          {tagname}
         </Wrapper>
       );
     case 'MEDIUM':
-      return <Wrapper $fontWeight={'400'}>{tagName}</Wrapper>;
+      return <Wrapper $fontWeight={'400'}>{tagname}</Wrapper>;
     case 'SMALL':
       return (
         <Wrapper
@@ -38,8 +61,9 @@ function BoardsTags({ mode, tagName }) {
           $height={'21px'}
           $fontSize={'11px'}
           $fontWeight={'350'}
+          onClick={onClick}
         >
-          {tagName}
+          {tagname}
         </Wrapper>
       );
   }
