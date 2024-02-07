06f02e194d9d0612d249a9d417d6410fa9037e69
황준식 <wnstlr0908@naver.com>
2024-02-07 12:31:03 +0900

    feat: 집사, 사용자의 공개방 목록 조회 axios  함수 생성

--- frontend/src/apis/api/room.js
+++ frontend/src/apis/api/room.js
@@ -36,4 +36,58 @@ const makeRoom = async (
   }
 };
 
-export { makeRoom };
+// 사용자 본인이 생성한 방 목록 구하기
+const getUserRoomList = async () =>
+  // 데이터 input 기입하기
+  {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: '/주소입력하기!',
+        data: {
+          userId,
+          subCategoryId,
+          title,
+          content,
+          place,
+          estimateDuration,
+          roomCreatedAt,
+          expectationStartedAt,
+          expectationEndedAt,
+          expectationPay,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+// 집사기준, 사용자들이 생성한 모든 방 목록 보여주기
+const getZipsaRoomList = async () =>
+  // 데이터 input 기입하기
+  {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: '/주소입력하기!',
+        data: {
+          userId,
+          subCategoryId,
+          title,
+          content,
+          place,
+          estimateDuration,
+          roomCreatedAt,
+          expectationStartedAt,
+          expectationEndedAt,
+          expectationPay,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+export { makeRoom, getUserRoomList, getZipsaRoomList };
