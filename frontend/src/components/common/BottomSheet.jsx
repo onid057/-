692cf9ae0c924c5c90c795f14349e4d2ab2ea6d0
황준식 <wnstlr0908@naver.com>
2024-02-07 18:05:20 +0900

    fix: roomID 변수 추가

--- frontend/src/components/common/BottomSheet.jsx
+++ frontend/src/components/common/BottomSheet.jsx
@@ -5,6 +5,7 @@ import GenderBadge from './GenderBadge';
 import GradeBadge from './GradeBadge';
 import PreferTag from './PreferTag';
 import { getZipsaListFromMap } from '../../apis/api/map';
+import { getZipsaListFromDetailInfo } from '../../apis/api/room';
 import { forwardRef, useState, useEffect } from 'react';
 
 const Wrapper = styled.div`
@@ -67,16 +68,26 @@ const Text = styled.div`
 // isOpen은 bottom sheet 자체가 열렸는지 확인하는 boolean prop
 // isDetailOpen은 bottom sheet 내에서 상세 정보가 열렸는지 확인하는 boolean prop
 const BottomSheet = forwardRef(
-  ({ isOpen, isDetailOpen, setIsDetailOpen, onClick, targetCluster }, ref) => {
+  (
+    { isOpen, isDetailOpen, setIsDetailOpen, onClick, targetCluster, roomId },
+    ref,
+  ) => {
     const [zipsaList, setZipsaList] = useState([]);
     const [targetZipsa, setTargetZipsa] = useState({}); // 현재 선택된 집사 한 명
 
     useEffect(() => {
-      if (targetCluster)
+      if (targetCluster) {
         getZipsaListFromMap(targetCluster.lat, targetCluster.lng).then(
           response => setZipsaList(response),
         );
-    }, [targetCluster]);
+      }
+      if (roomId) {
+        getZipsaListFromDetailInfo(roomId).then(response => {
+          setZipsaList(response);
+          console.log(response);
+        });
+      }
+    }, [targetCluster, roomId]);
 
     return (
       <Wrapper $isOpen={isOpen} $isDetailOpen={isDetailOpen} ref={ref}>
