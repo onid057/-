ed7293d31432f87f0913da1f1056353281e90604
황준식 <wnstlr0908@naver.com>
2024-02-07 18:04:45 +0900

    fix: 사용자 입장의 공개방 목록, 상세 페이지 조회 및 집사 리스트 조회 함수 구현

--- frontend/src/apis/api/room.js
+++ frontend/src/apis/api/room.js
@@ -37,31 +37,45 @@ const makeRoom = async (
 };
 
 // 사용자 본인이 생성한 방 목록 구하기
-const getUserRoomList = async () =>
+const getUserRoomList = async userId =>
   // 데이터 input 기입하기
   {
     try {
       const response = await axios({
         method: 'get',
-        url: '/주소입력하기!',
-        data: {
-          userId,
-          subCategoryId,
-          title,
-          content,
-          place,
-          estimateDuration,
-          roomCreatedAt,
-          expectationStartedAt,
-          expectationEndedAt,
-          expectationPay,
-        },
+        url: `/users/${userId}/rooms`,
       });
       return response.data;
     } catch (error) {
       console.log(error);
     }
   };
+
+const getRoomDetailInfo = async roomId => {
+  {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: `/rooms/${roomId}`,
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+const getZipsaListFromDetailInfo = async roomId => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `/rooms/${roomId}/notifications`,
+    });
+    return response.data.data.roomNotificationList;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // 집사기준, 사용자들이 생성한 모든 방 목록 보여주기
 const getZipsaRoomList = async () =>
@@ -71,18 +85,7 @@ const getZipsaRoomList = async () =>
       const response = await axios({
         method: 'get',
         url: '/주소입력하기!',
-        data: {
-          userId,
-          subCategoryId,
-          title,
-          content,
-          place,
-          estimateDuration,
-          roomCreatedAt,
-          expectationStartedAt,
-          expectationEndedAt,
-          expectationPay,
-        },
+        data: {},
       });
       return response.data;
     } catch (error) {
@@ -90,4 +93,10 @@ const getZipsaRoomList = async () =>
     }
   };
 
-export { makeRoom, getUserRoomList, getZipsaRoomList };
+export {
+  makeRoom,
+  getUserRoomList,
+  getZipsaRoomList,
+  getRoomDetailInfo,
+  getZipsaListFromDetailInfo,
+};
