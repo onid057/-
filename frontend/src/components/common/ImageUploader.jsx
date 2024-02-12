0006f1acf880f23a48abb85b912bdb1c27451c6d
junsik <wnstlr0908@naver.com>
2024-02-12 18:18:44 +0900

    fix: input 태그 단일 이미지만 받을 수 있도록 변경

--- frontend/src/components/common/ImageUploader.jsx
+++ frontend/src/components/common/ImageUploader.jsx
@@ -9,28 +9,10 @@ const ImageUploaderWrapper = styled.div`
   gap: 15px;
 `;
 
-function ImageUploader({ showImages, setShowImages, fileInputRef, children }) {
+function ImageUploader({ setImageFile, fileInputRef, children }) {
   const handleImageChange = event => {
-    const imageLists = event.target.files;
-    let imageUrlLists = [];
-
-    if (imageLists) {
-      // 여기에서 파일을 업로드하거나 다른 작업을 수행할 수 있다.
-      for (let i = 0; i < imageLists.length; i++) {
-        const currentImageUrl = URL.createObjectURL(imageLists[i]);
-        imageUrlLists.push(currentImageUrl);
-      }
-
-      // 최대 길이 3
-      if (imageUrlLists.length > 3) {
-        console.log(imageUrlLists);
-        alert('사진은 최대 3개까지 첨부 가능합니다.');
-        imageUrlLists = imageUrlLists.slice(0, 3);
-      }
-
-      // 이미지url 배열, 클릭했다는 것을 확인
-      setShowImages(imageUrlLists);
-    }
+    const image = event.target.files[0];
+    setImageFile(image);
   };
 
   return (
@@ -41,7 +23,6 @@ function ImageUploader({ showImages, setShowImages, fileInputRef, children }) {
         ref={fileInputRef}
         accept="image/*"
         style={{ display: 'none' }}
-        multiple
         onChange={handleImageChange}
       />
     </ImageUploaderWrapper>
