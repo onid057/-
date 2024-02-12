28afbf24fd0c63598de2e08b3f76bcc2d1352114
곽희웅 <gmldnd567@naver.com>
2024-02-12 15:25:22 +0900

    fix: SSEServiceImpl

--- backend/src/main/java/com/a407/back/model/service/SSEServiceImpl.java
+++ backend/src/main/java/com/a407/back/model/service/SSEServiceImpl.java
@@ -1,5 +1,4 @@
 package com.a407.back.model.service;
-
 import com.a407.back.domain.Room;
 import com.a407.back.domain.Zipsa;
 import com.a407.back.dto.notification.NotificationListResponse;
@@ -16,26 +15,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
 @Service
 @RequiredArgsConstructor
 public class SSEServiceImpl implements SSEService {
-
     private final Logger log = LoggerFactory.getLogger(this.getClass());
-
     @Value("${default.timeout}")
     private Long defaultTimeout;
-
     private final SSERepository sseRepository;
-
     private final UserRepository userRepository;
-
     private final ZipsaRepository zipsaRepository;
-
     private final RoomRepository roomRepository;
-
     private final CategoryRepository categoryRepository;
-
     // SseEmitter를 사용해서 알림을 보낼 때 사용
     public void send(Long userId) {
         // 로그인 한 유저의 SseEmitter 가져오기
@@ -63,16 +53,13 @@ public class SSEServiceImpl implements SSEService {
             }
         }, () -> log.error("No Emitter found"));
     }
-
     @Override
     public SseEmitter connect(Long userId) {
         SseEmitter sseEmitter = new SseEmitter(defaultTimeout);
         sseRepository.save(userId, sseEmitter);
-
         // 시간 초과, 네트워크 오류를 포함한 모든 이유로 비동기 요청이 정상 동작할 수 없을 때 저장해둔 SseEmitter를 삭제함.
         sseEmitter.onCompletion(() -> sseRepository.delete(userId));
         sseEmitter.onTimeout(() -> sseRepository.delete(userId));
-
         try {
             // 연결 요청에 의해 SseEmitter가 생성되면 더미 데이터를 보내줘야함.
             // 연결된 후 하나의 데이터도 전송되지 않는다면 SseEmitter의 유효시간이 끝났을 경우,
