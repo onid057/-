c7dc16b6fdf87dd93507ada159fa9321c86c2668
곽희웅 <gmldnd567@naver.com>
2024-02-12 17:41:58 +0900

    fix: sse

--- backend/src/main/java/com/a407/back/controller/SSEController.java
+++ backend/src/main/java/com/a407/back/controller/SSEController.java
@@ -2,7 +2,9 @@ package com.a407.back.controller;
 
 import com.a407.back.model.service.SSEService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,7 @@ public class SSEController {
     private final SSEService sseService;
 
     @GetMapping(value = "/sse/{userId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribeToNotificationList(@PathVariable("userId") Long userId) {
-        return sseService.connect(userId);
+    public ResponseEntity<SseEmitter> subscribeToNotificationList(@PathVariable("userId") Long userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(sseService.connect(userId));
     }
 }
