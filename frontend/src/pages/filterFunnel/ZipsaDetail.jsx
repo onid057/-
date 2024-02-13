a95c1821126fd131efb4d9a17620777da5dbe30a
이수민 <eelsm6@naver.com>
2024-02-13 13:22:59 +0900

    S10P12A407-785 feat: 집사 마이페이지 연결 구현

--- frontend/src/pages/filterFunnel/ZipsaDetail.jsx
+++ frontend/src/pages/filterFunnel/ZipsaDetail.jsx
@@ -1,53 +1,11 @@
 import styled from 'styled-components';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import NavigationBar from '../../components/common/NavigationBar';
 import Image from '../../components/common/Image';
 import ZipsaDetailProfile from '../../components/filter/ZipsaDetailProfile';
 import ZipsaDetailRoute from '../../components/filter/ZipsaDetailRoute';
-
-// 집사 상세정보를 서버에서 받아오는 부분?
-// 일단 나오는지 보려고 예시로 넣어놨어요
-const zipsaInfo = {
-  name: '피치피치어피치',
-  birth: '2024-01-23T14:25:12.000+00:00',
-  gender: 'man',
-  address: '서울시 강서구',
-  profileImage: null,
-  latitude: 37.54815556,
-  longitude: 126.851675,
-  gradeId: 1,
-  gradeName: '견습',
-  salary: 5000,
-  description: '우리는 친구친구 열심히 동네에서 친구 만들어봐요 오호홍',
-  preferTag: '산책하기#함께 장보기#병원 가기#자차 보유#요양보호사',
-  serviceCount: 1,
-  replyAverage: 15.2,
-  replyCount: 0,
-  kindnessAverage: 3.0,
-  skillAverage: 2.5,
-  rewindAverage: 4.2,
-  reviews: [
-    {
-      userName: '옆집사는 라이언',
-      profileImage: null,
-      content: '나쁘지 않았습니다.',
-      kindnessScore: 3,
-      skillScore: 5,
-      rewindScore: 7,
-      createdAt: '2024-01-23T14:25:13.000+00:00',
-    },
-    {
-      userName: '윗집사는 무지',
-      profileImage: null,
-      content: '음청좋았어요 짱짱짱 가나다라마바사',
-      kindnessScore: 3,
-      skillScore: 5,
-      rewindScore: 4,
-      createdAt: '2024-01-23T14:25:13.000+00:00',
-    },
-  ],
-  subCategory: ['병원 동행', '마트 동행', '식사 돌봄'],
-};
+import { getDetailZipsaInfo } from '../../apis/api/zipsaMyPage';
 
 const Wrapper = styled.div`
   width: 320px;
@@ -80,15 +38,32 @@ const NormalTextWrapper = styled.div`
   font-weight: normal;
 `;
 
-function ZipsaDetail({ onPrevious, onNext }) {
-  // 클릭 이벤트 핸들러 함수 정의
+function ZipsaDetail() {
+  // NavigationBar 사용 위한 변수 선언
+  const navigate = useNavigate();
+  const onPrevious = () => {
+    navigate(-1);
+  };
+
+  // (세부)집사 정보 조회 API 호출
+  const location = useLocation();
+  const helperId = location.state.helperId;
+  const [data, setData] = useState({});
+  console.log('ZipsaDetail에서의 helperId : ', helperId);
+
+  useEffect(() => {
+    getDetailZipsaInfo(helperId).then(response => {
+      console.log('(세부)집사 정보 조회 API 성공');
+      setData(response.data);
+      console.log(data);
+    });
+  }, []);
+
+  // 카테고리 변경 위한 변수 정의
   const [selectedCompo, setSelectedCompo] = useState('CATEGORY');
 
   const number =
-    (zipsaInfo.kindnessAverage +
-      zipsaInfo.skillAverage +
-      zipsaInfo.rewindAverage) /
-    3;
+    (data.kindnessAverage + data.skillAverage + data.rewindAverage) / 3;
   const avgScore = number.toFixed(2);
 
   return (
@@ -102,18 +77,16 @@ function ZipsaDetail({ onPrevious, onNext }) {
             src={process.env.PUBLIC_URL + '/images/left_arrow.svg'}
           ></Image>
         }
-        // rightContent는 없앴어요
         onPrevious={onPrevious}
-        onNext={onNext}
       ></NavigationBar>
 
-      <ZipsaDetailProfile
-        profileImage={zipsaInfo.profileImage}
-        name={zipsaInfo.name}
-        gradeId={zipsaInfo.gradeId}
+      {/* <ZipsaDetailProfile
+        profileImage={data.profileImage}
+        name={data.name}
+        gradeId={data.gradeId}
         avgScore={avgScore}
-        reviewCount={zipsaInfo.reviews.length}
-        description={zipsaInfo.description}
+        reviewCount={data.reviews.length}
+        description={data.description}
       ></ZipsaDetailProfile>
 
       <NavigationWrapper>
@@ -148,19 +121,18 @@ function ZipsaDetail({ onPrevious, onNext }) {
         )}
       </NavigationWrapper>
 
-      {/* 컴포넌트가 출력되는 부분 */}
       <ZipsaDetailRoute
         component={selectedCompo}
-        subCategory={zipsaInfo.subCategory}
-        preferTag={zipsaInfo.preferTag}
-        replyAverage={zipsaInfo.replyAverage}
-        serviceCount={zipsaInfo.serviceCount}
-        kindnessAverage={zipsaInfo.kindnessAverage}
-        skillAverage={zipsaInfo.skillAverage}
-        rewindAverage={zipsaInfo.rewindAverage}
-        totalReview={zipsaInfo.reviews.length}
-        reviews={zipsaInfo.reviews}
-      ></ZipsaDetailRoute>
+        subCategory={data.subCategory}
+        preferTag={data.preferTag}
+        replyAverage={data.replyAverage}
+        serviceCount={data.serviceCount}
+        kindnessAverage={data.kindnessAverage}
+        skillAverage={data.skillAverage}
+        rewindAverage={data.rewindAverage}
+        totalReview={data.reviews.length}
+        reviews={data.reviews}
+      ></ZipsaDetailRoute> */}
     </Wrapper>
   );
 }
