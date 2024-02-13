89b489bde1bb4e11065a2ca68e4de1bec9d7a0a7
곽희웅 <gmldnd567@naver.com>
2024-02-13 23:12:16 +0900

    fix: security

--- backend/src/main/java/com/a407/back/controller/AuthController.java
+++ backend/src/main/java/com/a407/back/controller/AuthController.java
@@ -63,7 +63,7 @@ public class AuthController {
 
 
             ResponseCookie accessCookie = ResponseCookie.from("Authorization", "accessCookie")
-                .sameSite("None").httpOnly(true).secure(true).path("/").domain("i10a407.p.ssafy.io")
+                .sameSite("None").httpOnly(true).secure(false).path("/").domain("i10a407.p.ssafy.io")
                 .maxAge(cookieMaxAge).build();
 
 //            ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", tokens.getRefreshToken())
